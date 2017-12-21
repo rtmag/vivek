12c63bdeb0763e86725b879b7920fea7814de96c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-12-22 01:15:28 +0800

    Update SRR_DW_creator.py

--- SRR_DW_creator.py
+++ SRR_DW_creator.py
@@ -1,1 +1,12 @@
+f=open("/Users/wone/Downloads/SRR_Acc_List.txt")
 
+line=f.readline()
+
+while line:
+
+    print "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/"+line[0:6]+"/00"+line[9:]+"/"+line+"/"+line+"_1.fastq.gz"
+
+    line=f.readline()
+
+
+f.close()
