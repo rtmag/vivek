3c2ac9cc42004e5717e7365c6749456bc76db5aa
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-01-11 20:59:01 +0800

    Update processing.py

--- processing.py
+++ processing.py
@@ -33,30 +33,32 @@ I=ChIP-Seq_H3K4me1_SKmel239.bam O=ChIP-Seq_H3K4me1_SKmel239_rmdup.bam  M=ChIP-Se
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
 I=ChIP-Seq_H3K4me1_SKmel2.bam O=ChIP-Seq_H3K4me1_SKmel2_rmdup.bam  M=ChIP-Seq_H3K4me1_SKmel2.mfile
+########
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=ChIP-Seq_H3K27ac_501MEL.bam O=ChIP-Seq_H3K27ac_501MEL_rmdup.bam  M=ChIP-Seq_H3K27ac_501MEL.mfile
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
+I=ChIP-Seq_H3K27ac_NHM1.bam O=ChIP-Seq_H3K27ac_NHM1_rmdup.bam  M=ChIP-Seq_H3K27ac_NHM1.mfile
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
+I=ChIP-Seq_H3K27ac_NHM2.bam O=ChIP-Seq_H3K27ac_NHM2_rmdup.bam  M=ChIP-Seq_H3K27ac_NHM2.mfile
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
+I=ChIP-Seq_H3K27ac_SKmel147.bam O=ChIP-Seq_H3K27ac_SKmel147_rmdup.bam  M=ChIP-Seq_H3K27ac_SKmel147.mfile
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
+I=ChIP-Seq_H3K27ac_SKmel239.bam O=ChIP-Seq_H3K27ac_SKmel239_rmdup.bam  M=ChIP-Seq_H3K27ac_SKmel239.mfile
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
+I=ChIP-Seq_H3K27ac_SKmel2.bam O=ChIP-Seq_H3K27ac_SKmel2_rmdup.bam  M=ChIP-Seq_H3K27ac_SKmel2.mfile
+##
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=ChIP-Seq_BRD4_NHM1.bam O=ChIP-Seq_BRD4_NHM1_rmdup.bam  M=ChIP-Seq_BRD4_NHM1.mfile
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
+I=ChIP-Seq_BRD4_SKmel147_2.bam O=ChIP-Seq_BRD4_SKmel147_2_rmdup.bam  M=ChIP-Seq_BRD4_SKmel147_2.mfile
 
 java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
+I=ChIP-Seq_BRD4_SKmel147.bam O=ChIP-Seq_BRD4_SKmel147_rmdup.bam  M=ChIP-Seq_BRD4_SKmel147.mfile
 
-java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
 
-java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
-I=.bam O=_rmdup.bam  M=.mfile
