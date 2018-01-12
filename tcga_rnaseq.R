3f6aef2d8cf9c23f22118fedcf6e349363db36a0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-01-12 17:48:34 +0800

    Update tcga_rnaseq.R

--- tcga_rnaseq.R
+++ tcga_rnaseq.R
@@ -14,10 +14,10 @@ colnames(sf3b1) <- rep("sf3b1",length(colnames(sf3b1)))
 ##
 countData = cbind(bap1, eif1ax, sf3b1)
 countData = head(countData,-5)
-colnames(countData) = make.names(colnames(countData),unique=T)
                                
 colData <- data.frame(group=colnames(countData))
-                               
+colnames(countData) = make.names(colnames(countData),unique=T)
+
 dds <- DESeqDataSetFromMatrix(
        countData = countData,
        colData = colData,
@@ -29,4 +29,29 @@ dLRT_res <- results(dLRT)
 
 pdf("Diagnostic_bygene_pca.pdf")
 plotPCA(dLRT_vsd,ntop=30000,intgroup=c('group'))
+dev.off()
+                               
+#
+
+
+
+colores=c("#bae1ff","#ffb3ba","#baffc9")
+clab=cbind(colores[track])
+
+ library(RColorBrewer)
+colors <- colorRampPalette( (brewer.pal(9, "GrRd")) )(20)
+
+
+# set the custom distance and clustering functions
+hclustfunc <- function(x) hclust(x, method="complete")
+distfunc <- function(x) dist(x, method="euclidean")
+
+ex1.clust=hclustfunc(distfunc(ex1))
+ex2.clust=hclustfunc(distfunc(ex2))
+
+pdf("CD41-_treated_VS_CD41+_treated.pdf")
+x=heatmap.3(rbind(ex1,ex2[ex2.clust$order,]),col=colors, hclustfun=hclustfunc, distfun=distfunc, 
+            scale="row", trace="none",cexCol=1,KeyValueName="Expression",Rowv=FALSE, RowSideColors=rlab,dendrogram="none")
+
+legend('topright',legend=c("Open Chromatin in CD41- treated","Open Chromatin in CD41+ treated"),fill=c("#bae1ff","#ffb3ba"),border=NA,bty = "n")
 dev.off()
