d218c6b66cc3ae8f83a7a7f325e3864367cd2acf
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-01-17 00:00:33 +0800

    Update tcga_rnaseq.R

--- tcga_rnaseq.R
+++ tcga_rnaseq.R
@@ -51,7 +51,7 @@ track[track=="eif1ax"]=2
 track[track=="sf3b1"]=3
 track=as.numeric(track)
                                
-colores=c("#bae1ff","#ffb3ba","#baffc9")
+colores=c("#ffb3ba","#baffc9","#bae1ff")
 clab=cbind(colores[track])
 colnames(clab)="Mutation"
                                
@@ -69,5 +69,65 @@ heatmap.3(ex,col=colors, hclustfun=hclustfunc, distfun=distfunc, labRow = FALSE,
             scale="row", trace="none",KeyValueName="Expression", ColSideColors=clab,dendrogram="both",margins = c(2, 2),
            cexRow=.6, cexCol=.6,keysize=0.9)
 
-legend(0,.9,legend=c("BAP1","EIF1AX","SF3B1"),fill=c("#bae1ff","#ffb3ba","#baffc9"),border=NA,bty = "n",cex=.9)
+legend(0,.9,legend=c("BAP1","EIF1AX","SF3B1"),fill=c("#ffb3ba","#baffc9","#bae1ff"),border=NA,bty = "n",cex=.9)
+dev.off()
+
+#############################################################
+#removing chr 3
+                               
+library(ensembldb)
+library("EnsDb.Hsapiens.v86")   
+edb <- EnsDb.Hsapiens.v86
+tx <- genes(edb, columns=c("gene_id", "gene_name"))
+
+
+tx_ex = tx[tx$gene_id %in% gsub("\\..+","",rownames(ex),perl=T),]                               
+tx_ex = tx_ex[order(tx_ex$gene_id)]
+ex = ex[order(rownames(ex)),]
+tx_xx=cbind(tx_ex$gene_id,as.character(tx_ex@seqnames))
+tx_xx=tx_xx[tx_xx[,2]!="3",]
+
+ex_xx = ex[gsub("\\..+","",rownames(ex),perl=T) %in% tx_xx[,1],]
+
+pdf("Diagnostic_bygene_noChr3_pca.pdf")
+plotPCA(dLRT_vsd[gsub("\\..+","",rownames(dLRT_vsd),perl=T) %in% tx_xx[,1],],ntop=30000,intgroup=c('group'))
+dev.off()
+                               
+par(mar=c(7,4,4,2)+0.1) 
+png(filename='vivek_heatmap_noChr3.png', width=800, height=750)  
+heatmap.3(ex_xx,col=colors, hclustfun=hclustfunc, distfun=distfunc, labRow = FALSE, labCol = FALSE,xlab="Tumor Sample", ylab="genes",
+            scale="row", trace="none",KeyValueName="Expression", ColSideColors=clab,dendrogram="both",margins = c(2, 2),
+           cexRow=.6, cexCol=.6,keysize=0.9)
+
+legend(0,.9,legend=c("BAP1","EIF1AX","SF3B1"),fill=c("#ffb3ba","#baffc9","#bae1ff"),border=NA,bty = "n",cex=.9)
+dev.off()
+
+##
+#####################
+colData[colData[,1]!='bap1',1]="sf3b1"
+                               
+dds <- DESeqDataSetFromMatrix(
+       countData = countData[gsub("\\..+","",rownames(countData),perl=T) %in% tx_xx[,1],],
+       colData = colData,
+       design = ~ group)
+                               
+dds_deseq <- DESeq(dds)
+res <- results(dds_deseq, contrast=c("group","bap1","sf3b1"))    
+write.table(rownames(res)[res$log2FoldChange>0],"genes_high_bap1.txt",sep="\t",row.names=F,col.names=F,quote=F)
+write.table(rownames(res)[res$log2FoldChange<0],"genes_high_EIF1AX_SF3B1.txt",sep="\t",row.names=F,col.names=F,quote=F)
+
+write.table(tx_ex$gene_name[tx_ex$gene_id %in% gsub("\\..+","",rownames(res)[res$log2FoldChange>0],perl=T)]
+,"genes_names_high_bap1.txt",sep="\t",row.names=F,col.names=F,quote=F)
+write.table(tx_ex$gene_name[tx_ex$gene_id %in% gsub("\\..+","",rownames(res)[res$log2FoldChange<0],perl=T)]
+,"genes_names_high_EIF1AX_SF3B1.txt",sep="\t",row.names=F,col.names=F,quote=F)
+
+#more result\(1\).csv|perl -pe 's/\,/\t/g'|perl -pe 's/\"//g'|cut -f2,7|head -n18 > ~/vivek/rna_tcga/enriched_high_bap1.txt    
+#
+x=read.table("enriched_high_bap1.txt",sep="\t",header=T)
+        
+pdf("bap1_highExpression.pdf",width=14, height=10)
+mar.default <- c(5,4,4,0) + 0.1
+par(mar = mar.default + c(0, 27, 0, 0)) 
+barplot(rev(-log10(x[,2])), border=F, horiz=T , las=1,names.arg=x[,1],col="#ffb3ba",xlab="-Log10 FDR",main="")
+abline(v=2,lty='dashed')
 dev.off()
