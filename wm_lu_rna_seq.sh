6f07972e3899f496727c6ba93e78933e6702e196
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-16 20:26:28 +0800

    Update wm_lu_rna_seq.sh

--- wm_lu_rna_seq.sh
+++ wm_lu_rna_seq.sh
@@ -1,1 +1,8 @@
 
+/home/sb/programfiles/STAR/bin/Linux_x86_64/STAR --genomeDir /home/roberto/references/star_hg38_gencode26_overhang149 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--readFilesIn /home/roberto/deepa/novogene/HCT116_siC_1.fq.gz \
+/home/roberto/deepa/novogene/HCT116_siC_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix /home/roberto/deepa/novogene/STAR/HCT116_siC_
