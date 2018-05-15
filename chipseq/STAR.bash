afac9c84209391d8c672bcc93295b520f4207740
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-15 17:56:13 +0800

    Update STAR.bash

--- chipseq/STAR.bash
+++ chipseq/STAR.bash
@@ -80,3 +80,104 @@ STAR --genomeDir /home/references/hg38/STAR_hg38_noAnnotation \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /home/rtm/vivek/chipseq/bam/INPUT_MP46_
 #################################################################
+#grep -P "Number of input reads|Uniquely mapped reads number |Uniquely mapped reads %" *Log.final.out| \
+#perl -pe 's/\s{2,}/\s/g'|perl -pe 's/\|/\t/g'
+#################################################################
+
+#ls -1 /home/rtm/vivek/chipseq/bam/*sortedByCoord.out.bam | perl -pe 's/(.+)\_Aligned.+/ \
+#picard MarkDuplicates REMOVE_DUPLICATES=true \\ \
+#I\=$1\_Aligned\.sortedByCoord\.out\.bam \\ \
+#O\=$1\_rmdup\.bam \\ \
+#M\=$1\_rmdup\.mfile
+#/g'
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/BAP1_Mel202_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/BAP1_Mel202_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/BAP1_Mel202_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/BAP1_Mel202_rmdup.bam
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/BAP1_P92-1_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/BAP1_P92-1_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/BAP1_P92-1_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/BAP1_P92-1_rmdup.bam
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/INPUT_Mel202_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/INPUT_Mel202_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/INPUT_Mel202_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/INPUT_Mel202_rmdup.bam
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/INPUT_MP46_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/INPUT_MP46_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/INPUT_MP46_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/INPUT_MP46_rmdup.bam
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/INPUT_P92-1_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/INPUT_P92-1_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/INPUT_P92-1_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/INPUT_P92-1_rmdup.bam
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/Miz1_MP46_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/Miz1_MP46_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/Miz1_MP46_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/Miz1_MP46_rmdup.bam
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/Miz1_P92-1_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/Miz1_P92-1_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/Miz1_P92-1_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/Miz1_P92-1_rmdup.bam
+
+picard MarkDuplicates REMOVE_DUPLICATES=true \ 
+I=/home/rtm/vivek/chipseq/bam/Myc_MP46_Aligned.sortedByCoord.out.bam \ 
+O=/home/rtm/vivek/chipseq/bam/Myc_MP46_rmdup.bam \ 
+M=/home/rtm/vivek/chipseq/bam/Myc_MP46_rmdup.mfile
+
+samtools index /home/rtm/vivek/chipseq/bam/Myc_MP46_rmdup.bam
+##############################################################################
+macs2 callpeak -f BAMPE -g hs -q 0.05 --keep-dup auto --call-summits -n Miz1_P92-1 --outdir /home/rtm/vivek/chipseq/macs2 \
+-t /home/rtm/vivek/chipseq/bam/Miz1_P92-1_rmdup.bam -c /home/rtm/vivek/chipseq/bam/INPUT_P92-1_rmdup.bam &
+
+macs2 callpeak -f BAMPE -g hs -q 0.05 --keep-dup auto --call-summits -n BAP1_P92-1 --outdir /home/rtm/vivek/chipseq/macs2 \
+-t /home/rtm/vivek/chipseq/bam/BAP1_P92-1_rmdup.bam -c /home/rtm/vivek/chipseq/bam/INPUT_P92-1_rmdup.bam &
+#
+macs2 callpeak -f BAMPE -g hs -q 0.05 --keep-dup auto --call-summits -n Miz1_MP46 --outdir /home/rtm/vivek/chipseq/macs2 \
+-t /home/rtm/vivek/chipseq/bam/Miz1_MP46_rmdup.bam -c /home/rtm/vivek/chipseq/bam/INPUT_MP46_rmdup.bam &
+
+macs2 callpeak -f BAMPE -g hs -q 0.05 --keep-dup auto --call-summits -n Myc_MP46 --outdir /home/rtm/vivek/chipseq/macs2 \
+-t /home/rtm/vivek/chipseq/bam/Myc_MP46_rmdup.bam -c /home/rtm/vivek/chipseq/bam/INPUT_MP46_rmdup.bam &
+#
+macs2 callpeak -f BAMPE -g hs -q 0.05 --keep-dup auto --call-summits -n BAP1_Mel202 --outdir /home/rtm/vivek/chipseq/macs2 \
+-t /home/rtm/vivek/chipseq/bam/BAP1_Mel202_rmdup.bam -c /home/rtm/vivek/chipseq/bam/INPUT_Mel202_rmdup.bam &
+##############################################################################
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/rtm/vivek/chipseq/bam/Miz1_P92-1_rmdup.bam \
+-o /home/rtm/vivek/chipseq/bw/Miz1_P92-1.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/rtm/vivek/chipseq/bam/BAP1_P92-1_rmdup.bam \
+-o /home/rtm/vivek/chipseq/bw/BAP1_P92-1.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/rtm/vivek/chipseq/bam/Miz1_MP46_rmdup.bam \
+-o /home/rtm/vivek/chipseq/bw/Miz1_MP46.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/rtm/vivek/chipseq/bam/Myc_MP46_rmdup.bam \
+-o /home/rtm/vivek/chipseq/bw/Myc_MP46.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM -b /home/rtm/vivek/chipseq/bam/BAP1_Mel202_rmdup.bam \
+-o /home/rtm/vivek/chipseq/bw/BAP1_Mel202.bw
+
+#################################################################################
+
+
