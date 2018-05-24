9eb73a0c5c8a809e44e3e75e413c3b4751a37ee1
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-25 00:00:20 +0800

    Create bam_to_bw.bash

---
+++ public_chipseq/bam_to_bw.bash
@@ -0,0 +1,37 @@
+####################################################################################################################################
+# ATAC
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ATAC-Seq_501MEL_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ATAC-Seq_501MEL.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ATAC-Seq_NHM1_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ATAC-Seq_NHM1.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ATAC-Seq_SKmel147_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ATAC-Seq_SKmel147.bw
+#############################################################################################################
+# H3K27ac
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K27ac_501MEL_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K27ac_501MEL.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K27ac_NHM1_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K27ac_NHM1.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K27ac_SKmel147_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K27ac_SKmel147.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K27ac_SKmel239_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K27ac_SKmel239.bw
+#############################################################################################################
+# H3K4me1
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K4me1_501MEL_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K4me1_501MEL.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K4me1_NHM1_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K4me1_NHM1.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K4me1_SKmel147_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K4me1_SKmel147.bw
+
+bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ChIP-Seq_H3K4me1_SKmel239_Aligned.sortedByCoord.out.bam \
+-o /root/vivek/public_chipseq/bw/ChIP-Seq_H3K4me1_SKmel239.bw
+######
