fc2cea21b5dc225412954acbc550103771936fe7
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-25 00:12:27 +0800

    Update bam_to_bw.bash

--- public_chipseq/bam_to_bw.bash
+++ public_chipseq/bam_to_bw.bash
@@ -1,3 +1,18 @@
+samtools index ATAC-Seq_501MEL_Aligned.sortedByCoord.out.bam &
+samtools index ATAC-Seq_NHM1_Aligned.sortedByCoord.out.bam &
+samtools index ATAC-Seq_SKmel147_Aligned.sortedByCoord.out.bam &
+
+samtools index ChIP-Seq_H3K27ac_501MEL_Aligned.sortedByCoord.out.bam &
+samtools index ChIP-Seq_H3K27ac_NHM1_Aligned.sortedByCoord.out.bam &
+samtools index ChIP-Seq_H3K27ac_SKmel147_Aligned.sortedByCoord.out.bam &
+samtools index ChIP-Seq_H3K27ac_SKmel239_Aligned.sortedByCoord.out.bam &
+
+samtools index ChIP-Seq_H3K4me1_501MEL_Aligned.sortedByCoord.out.bam &
+samtools index ChIP-Seq_H3K4me1_NHM1_Aligned.sortedByCoord.out.bam &
+samtools index ChIP-Seq_H3K4me1_SKmel147_Aligned.sortedByCoord.out.bam &
+samtools index ChIP-Seq_H3K4me1_SKmel239_Aligned.sortedByCoord.out.bam &
+
+
 ####################################################################################################################################
 # ATAC
 bamCoverage -p max -bs 10 --normalizeUsing CPM -b /root/vivek/public_chipseq/bam/ATAC-Seq_501MEL_Aligned.sortedByCoord.out.bam \
