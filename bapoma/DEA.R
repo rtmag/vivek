4369170838461c51997e6af848ef7e83dac7b2ed
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-25 10:02:13 +0800

    Update DEA.R

--- bapoma/DEA.R
+++ bapoma/DEA.R
@@ -35,4 +35,77 @@ colnames(dat)=c("b2","n2",
                 "b9","n9")
 
 saveRDS(dat,"bapoma_counts.rds")
-######################
+##############################################################################################################
+
+countData=readRDS("bapoma_counts.rds")
+options(scipen=999)
+library(DESeq2)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
+design<-data.frame(patient = c("2","2","5","5","7","7","81","81","82","82","9","9"),
+                    status = c("bap","norm","bap","norm","bap","norm","bap","norm","bap","norm","bap","norm") )
+
+dds <- DESeqDataSetFromMatrix(countData = countData, colData = design, design = ~ patient + status )
+dds <- DESeq(dds)
+dds_vsd <- varianceStabilizingTransformation(dds)
+dds_res = results(dds)
+vsd = assay(dds_vsd)
+
+pdf("Diagnostic_pca_all_samples.pdf")
+plotPCA(dds_vsd,ntop=50000,intgroup=c("status"))
+dev.off()
+
+saveRDS(vsd,"bapoma_vsd.rds")
+##############################################################################################################
+A = "Normal"
+B = "Bapoma"
+  # Volcano
+  title= "norm_vs_bap_volcano.pdf"
+  pdf(title)
+  plot(dds_res$log2FoldChange,-log10(dds_res$padj),xlab=expression('Log'[2]*paste(' Fold Change ')),
+              ylab=expression('-Log'[10]*' Q-values'),col=alpha("grey",.5),pch=20 )
+  abline(v=-1,lty = 2,col="grey")
+  abline(v=1,lty = 2,col="grey")
+  abline(h=-log10(0.05),lty = 2,col="grey")
+  points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>1 & dds_res$padj<0.05],
+       -log10(dds_res$padj)[abs(dds_res$log2FoldChange)>1 & dds_res$padj<0.05],
+      col="red",pch=20)
+  legend("topright", paste(A,":",length(which(dds_res$log2FoldChange>1 & dds_res$padj<0.05))), bty="n") 
+  legend("topleft", paste(B,":",length(which(dds_res$log2FoldChange<(-1) & dds_res$padj<0.05))), bty="n") 
+  dev.off()
+  # Heatmap
+  title= "norm_vs_bap_heatmap.ps"
+  postscript(title,height=10,width=10,horizontal=F)
+  sig_vsd = vsd[which(abs(dds_res$log2FoldChange)>1 & dds_res$padj<0.05),]
+  colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+  heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  labRow = FALSE,xlab="", ylab="Genes",key.title="Gene expression",cexCol=.8)
+  dev.off()
+# MAplot
+  title= "norm_vs_bap_maplot.pdf"
+  pdf(title)
+  plotMA(dds_res)
+  dev.off()
+# RankedListFor GSEA
+  up_reg = dds_res[ which(dds_res$log2FoldChange>0),]
+  up_reg = up_reg[ !is.na(up_reg$padj),]
+  up_reg_log=-log(up_reg$padj)
+  names(up_reg_log) = rownames(up_reg)
+  dw_reg = dds_res[ which(dds_res$log2FoldChange<0),]
+  dw_reg = dw_reg[ !is.na(dw_reg$padj),]
+  dw_reg_log=log(dw_reg$padj)
+  names(dw_reg_log) = rownames(dw_reg)
+
+  rankedlist = cbind(sort(c(up_reg_log,dw_reg_log),decreasing=T) )
+  rankedlist = data.frame(ensid=rownames(rankedlist), log10FDR=rankedlist)
+  title= "norm_vs_bap_genes_ranked_table_FCFDR.rnk"
+  write.table(rankedlist,title, sep="\t", quote=F,col.names=F,row.names=F)
+# Significant Results ordered by log2FC
+  csv_table = dds_res[which(dds_res$padj<0.05 & abs(dds_res$log2FoldChange)>1),]
+  csv_table = csv_table[order(csv_table$log2FoldChange),]
+  title= "norm_vs_bap_differentially_expressed_genes.csv"
+  write.csv(csv_table,title)
+
+#############################################################################################################
