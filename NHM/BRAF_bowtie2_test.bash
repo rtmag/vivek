de7459374a45473f34aaab6aea5143ffcdf3a7db
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-02 00:50:08 +0800

    Update BRAF_bowtie2_test.bash

--- NHM/BRAF_bowtie2_test.bash
+++ NHM/BRAF_bowtie2_test.bash
@@ -1,15 +1,15 @@
 bowtie2 -p 40 -x /root/resources/hg38_bowtie2/hg38 \
 -1 <(zcat /root/vivek/vivek_new_rna_seq/fastq/B1_S38_R1_001.fastq.gz) \
 -2 <(zcat /root/vivek/vivek_new_rna_seq/fastq/B1_S38_R2_001.fastq.gz) \
--S /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_1.sam
+-S /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_1.sam &> BRAF_1.log
 
 samtools view -bS /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_1.sam > /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_1.bam
-rm /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_1.sam
+rm /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_1.sam 
 #
 bowtie2 -p 40 -x /root/resources/hg38_bowtie2/hg38 \
 -1 <(zcat /root/vivek/vivek_new_rna_seq/fastq/B2_S39_R1_001.fastq.gz) \
 -2 <(zcat /root/vivek/vivek_new_rna_seq/fastq/B2_S39_R2_001.fastq.gz) \
--S /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_2.sam
+-S /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_2.sam &> BRAF_2.log
 
 samtools view -bS /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_2.sam > /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_2.bam
 rm /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_2.sam
@@ -17,7 +17,7 @@ rm /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_2.sam
 bowtie2 -p 40 -x /root/resources/hg38_bowtie2/hg38 \
 -1 <(zcat /root/vivek/vivek_new_rna_seq/fastq/B3_S40_R1_001.fastq.gz) \
 -2 <(zcat /root/vivek/vivek_new_rna_seq/fastq/B3_S40_R2_001.fastq.gz) \
--S /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_3.sam
+-S /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_3.sam &> BRAF_3.log
 
 samtools view -bS /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_3.sam > /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_3.bam
 rm /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_3.sam
@@ -25,7 +25,7 @@ rm /root/vivek/vivek_new_rna_seq/bowtie2/BRAF_3.sam
 bowtie2 -p 40 -x /root/resources/hg38_bowtie2/hg38 \
 -1 <(zcat /root/vivek/vivek_new_rna_seq/fastq/D1_NHMbplusc__S44_R1_001.fastq.gz) \
 -2 <(zcat /root/vivek/vivek_new_rna_seq/fastq/D1_NHMbplusc__S44_R2_001.fastq.gz) \
--S /root/vivek/vivek_new_rna_seq/bowtie2/B_C_1.sam
+-S /root/vivek/vivek_new_rna_seq/bowtie2/B_C_1.sam &> BC_1.log
 
 samtools view -bS /root/vivek/vivek_new_rna_seq/bowtie2/B_C_1.sam > /root/vivek/vivek_new_rna_seq/bowtie2/B_C_1.bam
 rm /root/vivek/vivek_new_rna_seq/bowtie2/B_C_1.sam
@@ -33,7 +33,7 @@ rm /root/vivek/vivek_new_rna_seq/bowtie2/B_C_1.sam
 bowtie2 -p 40 -x /root/resources/hg38_bowtie2/hg38 \
 -1 <(zcat /root/vivek/vivek_new_rna_seq/fastq/D2_NHMbplusc__S45_R1_001.fastq.gz) \
 -2 <(zcat /root/vivek/vivek_new_rna_seq/fastq/D2_NHMbplusc__S45_R2_001.fastq.gz) \
--S /root/vivek/vivek_new_rna_seq/bowtie2/B_C_2.sam
+-S /root/vivek/vivek_new_rna_seq/bowtie2/B_C_2.sam &> BC_2.log
 
 samtools view -bS /root/vivek/vivek_new_rna_seq/bowtie2/B_C_2.sam > /root/vivek/vivek_new_rna_seq/bowtie2/B_C_2.bam
 rm /root/vivek/vivek_new_rna_seq/bowtie2/B_C_2.sam
@@ -41,7 +41,7 @@ rm /root/vivek/vivek_new_rna_seq/bowtie2/B_C_2.sam
 bowtie2 -p 40 -x /root/resources/hg38_bowtie2/hg38 \
 -1 <(zcat /root/vivek/vivek_new_rna_seq/fastq/D3_NHMbplusc__S46_R1_001.fastq.gz) \
 -2 <(zcat /root/vivek/vivek_new_rna_seq/fastq/D3_NHMbplusc__S46_R2_001.fastq.gz) \
--S /root/vivek/vivek_new_rna_seq/bowtie2/B_C_3.sam
+-S /root/vivek/vivek_new_rna_seq/bowtie2/B_C_3.sam &> BC_3.log
 
 samtools view -bS /root/vivek/vivek_new_rna_seq/bowtie2/B_C_3.sam > /root/vivek/vivek_new_rna_seq/bowtie2/B_C_3.bam
 rm /root/vivek/vivek_new_rna_seq/bowtie2/B_C_3.sam
