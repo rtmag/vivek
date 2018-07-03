b8910a13b28f15b7ab1c1ec0d38303f28a8638de
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-04 01:03:08 +0800

    Create chipseq_histone.bash

---
+++ chipseq/chipseq_histone.bash
@@ -0,0 +1,23 @@
+################################################################################################################################
+# Cleanup
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-01_S60_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-02_S46_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-03_S47_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-04_S56_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-05_S61_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-06_S48_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-07_S49_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-08_S57_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-09_S62_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-10_S50_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-11_S51_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-12_S58_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-13_S52_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-14_S63_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-15_S53_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-16_S54_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-17_S59_R1_001.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o /root/vivek/chip-seq/trimmed/ /root/vivek/chip-seq/fastq/Chip-18_S55_R1_001.fastq.gz &
+################################################################################################################################
+
+
