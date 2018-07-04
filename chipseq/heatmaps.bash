a0c1413782e1eae9e46be0113004ed09d8206265
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-05 03:33:30 +0800

    Update heatmaps.bash

--- chipseq/heatmaps.bash
+++ chipseq/heatmaps.bash
@@ -108,3 +108,69 @@ computeMatrix reference-point \
 -R /root/vivek/vivek_new_rna_seq/deseq2/NHM_CDKN2A.bed --referencePoint center \
 --sortRegions descend -bs 20 -a 3000 -b 3000 -p max -out /root/vivek/chip-seq/heatmap/H3K9me3_NHM_CDKN2A.mat
 ################################################################################################################################
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Blues \
+-m /root/vivek/chip-seq/heatmap/H3K27ac_NHM_BRAF_CDKN2A.mat \
+ --samplesLabel "H3K27ac BRAF" "H3K27ac CDKN2A+BRAF" "H3K27ac CDKN2A" "H3K27ac NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K27ac_NHM_BRAF_CDKN2A.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Oranges \
+-m /root/vivek/chip-seq/heatmap/H3K27me3_NHM_BRAF_CDKN2A.mat \
+ --samplesLabel "H3K27me3 BRAF" "H3K27me3 CDKN2A+BRAF" "H3K27me3 CDKN2A" "H3K27me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K27me3_NHM_BRAF_CDKN2A.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Greens \
+-m /root/vivek/chip-seq/heatmap/H3K4me3_NHM_BRAF_CDKN2A.mat \
+ --samplesLabel "H3K4me3 BRAF" "H3K4me3 CDKN2A+BRAF" "H3K4me3 CDKN2A" "H3K4me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K4me3_NHM_BRAF_CDKN2A.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Reds \
+-m /root/vivek/chip-seq/heatmap/H3K9me3_NHM_BRAF_CDKN2A.mat \
+ --samplesLabel "H3K9me3 BRAF" "H3K9me3 CDKN2A+BRAF" "H3K9me3 CDKN2A" "H3K9me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K9me3_NHM_BRAF_CDKN2A.pdf
+################################################################################################
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Blues \
+-m /root/vivek/chip-seq/heatmap/H3K27ac_NHM_BRAF.mat \
+ --samplesLabel "H3K27ac BRAF" "H3K27ac CDKN2A+BRAF" "H3K27ac CDKN2A" "H3K27ac NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K27ac_NHM_BRAF.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Oranges \
+-m /root/vivek/chip-seq/heatmap/H3K27me3_NHM_BRAF.mat \
+ --samplesLabel "H3K27me3 BRAF" "H3K27me3 CDKN2A+BRAF" "H3K27me3 CDKN2A" "H3K27me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K27me3_NHM_BRAF.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Greens \
+-m /root/vivek/chip-seq/heatmap/H3K4me3_NHM_BRAF.mat \
+ --samplesLabel "H3K4me3 BRAF" "H3K4me3 CDKN2A+BRAF" "H3K4me3 CDKN2A" "H3K4me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K4me3_NHM_BRAF.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Reds \
+-m /root/vivek/chip-seq/heatmap/H3K9me3_NHM_BRAF.mat \
+ --samplesLabel "H3K9me3 BRAF" "H3K9me3 CDKN2A+BRAF" "H3K9me3 CDKN2A" "H3K9me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K9me3_NHM_BRAF.pdf
+
+#############################################################################################################
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Blues \
+-m /root/vivek/chip-seq/heatmap/H3K27ac_NHM_CDKN2A.mat \
+ --samplesLabel "H3K27ac BRAF" "H3K27ac CDKN2A+BRAF" "H3K27ac CDKN2A" "H3K27ac NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K27ac_NHM_CDKN2A.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Oranges \
+-m /root/vivek/chip-seq/heatmap/H3K27me3_NHM_CDKN2A.mat \
+ --samplesLabel "H3K27me3 BRAF" "H3K27me3 CDKN2A+BRAF" "H3K27me3 CDKN2A" "H3K27me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K27me3_NHM_CDKN2A.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Greens \
+-m /root/vivek/chip-seq/heatmap/H3K4me3_NHM_CDKN2A.mat \
+ --samplesLabel "H3K4me3 BRAF" "H3K4me3 CDKN2A+BRAF" "H3K4me3 CDKN2A" "H3K4me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K4me3_NHM_CDKN2A.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Reds \
+-m /root/vivek/chip-seq/heatmap/H3K9me3_NHM_CDKN2A.mat \
+ --samplesLabel "H3K9me3 BRAF" "H3K9me3 CDKN2A+BRAF" "H3K9me3 CDKN2A" "H3K9me3 NHM" \
+-out /root/ong_dukenus/ATAC-SEQ/heatmap/H3K9me3_NHM_CDKN2A.pdf
+
+
+#
+pdfjam ach2azOnly_20bp_2kb_CPM_hg38_TSS_K4_byAcH2AZ.pdf h2azOnly_20bp_2kb_CPM_hg38_TSS_K4_byAcH2AZ.pdf --nup 2x1 --landscape
