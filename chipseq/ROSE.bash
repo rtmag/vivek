ef621ba40024be7d35e718e78946bf7286c27589
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-02 16:16:04 +0800

    Update ROSE.bash

--- chipseq/ROSE.bash
+++ chipseq/ROSE.bash
@@ -1,6 +1,8 @@
+cut -f 1-6 /root/vivek/chip-seq/macs2/NHM_H3K27ac_peaks.broadPeak > /root/vivek/chip-seq/macs2/NHM_H3K27ac_peaks.bed
+
 python /root/myPrograms/rose/ROSE_main_hg38.py \
 -r /root/vivek/chip-seq/bam/NHM_H3K27ac_rmdup.bam \
 -c /root/vivek/chip-seq/bam/NHM_input_rmdup.bam \
--i /root/vivek/chip-seq/macs2/NHM_H3K27ac_peaks.broadPeak \
+-i /root/vivek/chip-seq/macs2/NHM_H3K27ac_peaks.bed \
 -g HG38 \
--o /root/vivek/chip-seq/ROSE/NHM
+-o /root/vivek/chip-seq/ROSE/NHM &> NHM.log
