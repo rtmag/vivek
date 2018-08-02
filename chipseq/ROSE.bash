f6e97d5587045126ef9490fdfd6280aab8c9dfaa
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-02 16:07:34 +0800

    Create ROSE.bash

---
+++ chipseq/ROSE.bash
@@ -0,0 +1,6 @@
+python /root/myPrograms/rose/ROSE_main_hg38.py \
+-r /root/vivek/chip-seq/bam/NHM_H3K27ac_rmdup.bam \
+-c /root/vivek/chip-seq/bam/NHM_input_rmdup.bam \
+-i /root/vivek/chip-seq/macs2/NHM_H3K27ac_peaks.broadPeak \
+-g HG38 \
+-o /root/vivek/chip-seq/ROSE/NHM
