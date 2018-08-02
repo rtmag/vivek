182ab6b389003c6542c9e6ade42e87ee56c46d45
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-02 18:08:47 +0800

    Update chromHMM.bash

--- chipseq/chromHMM.bash
+++ chipseq/chromHMM.bash
@@ -3,3 +3,10 @@ java -mx22000M -jar /root/myPrograms/ChromHMM/ChromHMM.jar BinarizeBam \
 /root/vivek/chip-seq/bam/ \
 vivek_nhm_input_matrix.txt \
 binarizedBAM
+
+java -mx22000M -jar /root/myPrograms/ChromHMM/ChromHMM.jar LearnModel -p 22 \
+/root/vivek/chip-seq/chromHMM/binarizedBAM \
+/root/vivek/chip-seq/chromHMM/LearnModel \
+14 \
+hg38
+
