ccdb4110a8752f90761047e4fd2987a95e4ef9c3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-03 01:05:47 +0800

    Update chromHMM.bash

--- chipseq/chromHMM.bash
+++ chipseq/chromHMM.bash
@@ -19,7 +19,7 @@ java -mx92000M -jar /root/myPrograms/ChromHMM/ChromHMM.jar BinarizeBam \
 vivek_nhm_input_matrix2.txt \
 binarizedBAM2
 
-java -mx52000M -jar /root/myPrograms/ChromHMM/ChromHMM.jar LearnModel -p 0 binarizedBAM LearnModel2 10 hg38
+java -mx52000M -jar /root/myPrograms/ChromHMM/ChromHMM.jar LearnModel -p 0 binarizedBAM2 LearnModel2 10 hg38
 ####################################################################################################################################
 
 java -mx92000M -jar /root/myPrograms/ChromHMM/ChromHMM.jar BinarizeBam \
@@ -30,3 +30,7 @@ NHM_only_binarizedBAM
 
 java -mx52000M -jar /root/myPrograms/ChromHMM/ChromHMM.jar LearnModel -p 0 NHM_only_binarizedBAM NHM_only_LearnModel 10 hg38
 ####################################################################################################################################
+
+java -mx22000M -jar ChromHMM.jar BinarizeBam hg38.chrom.sizes bam/ input_matrix.txt binarizedBAM
+java -mx52000M -jar ChromHMM.jar LearnModel -p 0 binarizedBAM/ LearnModel/ 10 hg38
+
