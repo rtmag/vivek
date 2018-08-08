afeb937442c4afbeaa7dd25e47ce1f6d4e9f5c46
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-09 04:28:13 +0800

    Update ROSE.bash

--- chipseq/ROSE.bash
+++ chipseq/ROSE.bash
@@ -7,3 +7,21 @@ python /root/myPrograms/rose/ROSE_main_hg38.py \
 -g HG38 \
 -o /root/vivek/chip-seq/ROSE/NHM &> NHM.log
 # NOTES WORST PROGRAM EVER, RUN INSIDE THE DIRECTORY WITH THE SCRIPTS...
+################################################################################
+cut -f 1-6 /root/vivek/chip-seq/macs2/BRAF_H3K27ac_peaks.broadPeak > /root/vivek/chip-seq/macs2/BRAF_H3K27ac_peaks.bed
+
+python /root/myPrograms/rose/ROSE_main_hg38.py \
+-r /root/vivek/chip-seq/bam/BRAF_H3K27ac_rmdup.bam \
+-c /root/vivek/chip-seq/bam/CDKN2A+BRAF_input_rmdup.bam \
+-i /root/vivek/chip-seq/macs2/BRAF_H3K27ac_peaks.bed \
+-g HG38 \
+-o /root/vivek/chip-seq/ROSE/BRAF &> BRAF.log
+################################################################################
+cut -f 1-6 /root/vivek/chip-seq/macs2/CDKN2A+BRAF_H3K27ac_peaks.broadPeak > /root/vivek/chip-seq/macs2/CDKN2A+BRAF_H3K27ac_peaks.bed
+
+python /root/myPrograms/rose/ROSE_main_hg38.py \
+-r /root/vivek/chip-seq/bam/CDKN2A+BRAF_H3K27ac_rmdup.bam \
+-c /root/vivek/chip-seq/bam/CDKN2A+BRAF_input_rmdup.bam \
+-i /root/vivek/chip-seq/macs2/CDKN2A+BRAF_H3K27ac_peaks.bed \
+-g HG38 \
+-o /root/vivek/chip-seq/ROSE/CDKN2A+BRAF &> CDKN2A+BRAF.log
