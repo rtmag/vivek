bbe706682a8d9ba34b5767c02ae824bbf999f944
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-13 23:37:37 +0800

    Update chromHMM_rna.R

--- chipseq/chromHMM_rna.R
+++ chipseq/chromHMM_rna.R
@@ -13,7 +13,13 @@ library(RColorBrewer)
 
 sig_vsd = integra[,c(10,11,12,1,2,3,4,5,6)]
 sig_vsd = sig_vsd[complete.cases(sig_vsd),]
+rownames(sig_vsd)=NULL
+
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
   colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+postscript("chromHMM_rna.ps")
   heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-  xlab="", ylab="",key.title="Gene expression",cexCol=.65,cexRow=.2)
+  xlab="", ylab="",key.title="Gene expression",cexCol=.65,dendrogram='none',     
+Rowv=FALSE,
+Colv=FALSE)
+dev.off()
