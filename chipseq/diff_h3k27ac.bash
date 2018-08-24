fd7d5afc9ee6d3fac4d57412a65a3604704d28ab
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-24 19:39:45 +0800

    Update diff_h3k27ac.bash

--- chipseq/diff_h3k27ac.bash
+++ chipseq/diff_h3k27ac.bash
@@ -6,18 +6,18 @@ bamToBed -i bam/NHM_H3K27ac_rmdup.bam > bed/NHM_H3K27ac.bed &
 
 diffReps.pl --treatment ./bed/CDKN2A+BRAF_H3K27ac.bed \
 --control ./bed/NHM_H3K27ac.bed \
---meth gt --gname hg38 --mode n --report /root/vivek/chip-seq/diffreps/CDKN2A+BRAF_vs_NHM --nproc 60 --noanno --nohs
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode n --report /root/vivek/chip-seq/diffreps/CDKN2A+BRAF_vs_NHM --nproc 60 --noanno --nohs
 
 diffReps.pl --treatment ./bed/BRAF_H3K27ac.bed \
 --control ./bed/NHM_H3K27ac.bed \
---meth gt --gname hg38 --mode n --report /root/vivek/chip-seq/diffreps/BRAF_vs_NHM --nproc 60 --noanno --nohs
+--meth gt ---chrlen ~/resources/hg38.chrom.sizes --mode n --report /root/vivek/chip-seq/diffreps/BRAF_vs_NHM --nproc 60 --noanno --nohs
 
 diffReps.pl --treatment ./bed/CDKN2A+BRAF_H3K27ac.bed \
 --control ./bed/NHM_H3K27ac.bed \
---meth gt --gname hg38 --mode p --report /root/vivek/chip-seq/diffreps/CDKN2A+BRAF_vs_NHM --nproc 60 --noanno --nohs
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode p --report /root/vivek/chip-seq/diffreps/CDKN2A+BRAF_vs_NHM --nproc 60 --noanno --nohs
 
 diffReps.pl --treatment ./bed/BRAF_H3K27ac.bed \
 --control ./bed/NHM_H3K27ac.bed \
---meth gt --gname hg38 --mode p --report /root/vivek/chip-seq/diffreps/BRAF_vs_NHM --nproc 60 --noanno --nohs
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode p --report /root/vivek/chip-seq/diffreps/BRAF_vs_NHM --nproc 60 --noanno --nohs
 
 ##########################################################################
