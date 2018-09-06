aa0e1abd7bb3f99c879fe4b17a719b2427555c4e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-06 23:57:11 +0800

    Update rnaseq_mfuzz.R

--- NHM/rnaseq_mfuzz.R
+++ NHM/rnaseq_mfuzz.R
@@ -5,3 +5,57 @@ library(DESeq2)
 library(gplots)
 library(factoextra)
 library(RColorBrewer)
+
+require(Biobase)
+library(Mfuzz)
+library(ggplot2)
+options(bitmapType="cairo")
+
+design<-data.frame(group=c("BRAF","BRAF","BRAF",
+                "B_C","B_C","B_C",
+                "CDKN2A","CDKN2A","CDKN2A",
+                "NHM","NHM","NHM") )
+
+dLRT <- DESeqDataSetFromMatrix(countData = countData, colData = design, design = ~ group )
+dLRT <- DESeq(dLRT, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+dLRT_res = results(dLRT)
+vsd = assay(dLRT_vsd)
+
+
+############################################################################################################################################
+sig_vsd = data.frame(NHM=rowMeans(vsd[which(dLRT_res$padj<0.00001),10:12]),BRAF=rowMeans(vsd[which(dLRT_res$padj<0.00001),1:3]),
+                     CDKN2A=rowMeans(vsd[which(dLRT_res$padj<0.00001),7:9]),B_C=rowMeans(vsd[which(dLRT_res$padj<0.00001),4:6]))
+
+wt<-new("ExpressionSet", exprs=as.matrix(sig_vsd))
+wt.s<-standardise(wt)
+cl_wt<-mfuzz(wt.s,c=9,m=mestimate(wt.s))
+pdf('RNASEQ_mfuzz_FDR1e-5.pdf')
+mfuzz.plot(wt.s,cl=cl_wt,mfrow=c(3,3),new.window=F,time.labels=c("NHM","BRAF","CDKN2A","B+C"))
+dev.off()
+table(cl_wt$cluster)
+
+############################################################################################################################################
+sig_vsd = data.frame(NHM=rowMeans(vsd[which(dLRT_res$padj<0.05),10:12]),BRAF=rowMeans(vsd[which(dLRT_res$padj<0.05),1:3]),
+                     CDKN2A=rowMeans(vsd[which(dLRT_res$padj<0.05),7:9]),B_C=rowMeans(vsd[which(dLRT_res$padj<0.05),4:6]))
+
+wt<-new("ExpressionSet", exprs=as.matrix(sig_vsd))
+wt.s<-standardise(wt)
+cl_wt<-mfuzz(wt.s,c=9,m=mestimate(wt.s))
+pdf('RNASEQ_mfuzz_FDR5.pdf')
+mfuzz.plot(wt.s,cl=cl_wt,mfrow=c(3,3),new.window=F,time.labels=c("NHM","BRAF","CDKN2A","B+C"))
+dev.off()
+table(cl_wt$cluster)
+
+
+write.table(names(cl_wt$cluster[cl_wt$cluster==1]) ,"ct_1_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==2]) ,"ct_2_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==3]) ,"ct_3_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==4]) ,"ct_4_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==5]) ,"ct_5_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==6]) ,"ct_6_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==7]) ,"ct_7_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==8]) ,"ct_8_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(names(cl_wt$cluster[cl_wt$cluster==9]) ,"ct_9_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
+
+########################################################
