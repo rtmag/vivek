ce0c200b297a04f4599d9381e672fc4d0eee128a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-15 04:12:38 +0800

    Update superEnhancer_redo.R

--- chipseq/superEnhancer_redo.R
+++ chipseq/superEnhancer_redo.R
@@ -7,14 +7,15 @@ bedtools merge -i - > superEnhancer_redo_merge.bed
 ##################################################################
 library(Rsubread)
 options(scipen=999)
+library(DESeq2)
 
-x=read.table('/root/vivek/chip-seq/ROSE/SE_redo/superEnhancer_redo_merge.bed',sep="\t",stringsAsFactors=F)
+
+x=read.table('NHM_BC_sp.bed',sep="\t",stringsAsFactors=F)
 
 ann = data.frame(GeneID=paste(x[,1],x[,2],x[,3],sep="_!_"),Chr=x[,1],Start=x[,2],End=x[,3],Strand='+')
 
 bam.files <- c('/root/vivek/chip-seq/bam/NHM_H3K27ac_rmdup.bam',
               '/root/vivek/chip-seq/bam/BRAF_H3K27ac_rmdup.bam',
-              '/root/vivek/chip-seq/bam/CDKN2A_H3K27ac_rmdup.bam',
               '/root/vivek/chip-seq/bam/CDKN2A+BRAF_H3K27ac_rmdup.bam')
 
 fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=60)
@@ -22,7 +23,7 @@ fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=60)
 x=fc_SE$counts
 colnames(x) = c("NHM","BRAF","CDKN2A","CB")
 saveRDS(x,"superEnhancer_counts.rds")
-
+######
 library(DESeq2)
 x= readRDS("superEnhancer_counts.rds")
 
@@ -39,3 +40,5 @@ sig=counts[fc>1.6,]
 sig = sig[complete.cases(sig),]
 bed = rownames(sig[complete.cases(sig),])
 write.table(gsub("_!_","\t",bed,perl=T),"Significant_Open_60percent_CB_SE.bed",sep="\t",quote=F,row.name=F,col.name=F)
+
+
