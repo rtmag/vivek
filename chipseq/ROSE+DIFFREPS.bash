8217353877136ee4961a342fa20e41c5fb80048f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-15 04:04:52 +0800

    Update ROSE+DIFFREPS.bash

--- chipseq/ROSE+DIFFREPS.bash
+++ chipseq/ROSE+DIFFREPS.bash
@@ -1,8 +1,24 @@
 more BC_VS_BRAF_1pro |grep -v "#"|grep -v "Treatment.cnt"|cut -f1,2,3,11 > BC_VS_BRAF_1pro.bed
 more BC_VS_NHM_1pro |grep -v "#"|grep -v "Treatment.cnt"|cut -f1,2,3,11 > BC_VS_NHM_1pro.bed
 
-bedtools intersect -a ../NHM_40/NHM_H3K27ac_40_Gateway_SuperEnhancers.bed -b ../../macs2/NHM_H3K27ac_40_noTSS.bed| \
-bedtools intersect -a BC_VS_NHM_1pro.bed  -b -|grep Down > NHM_sp_beta.bed
+grep "Down" BC_VS_NHM_1pro.bed > BC_VS_NHM_1pro_down.bed
+grep "Up" BC_VS_NHM_1pro.bed > BC_VS_NHM_1pro_up.bed
 
-bedtools intersect -a ../CDKN2A+BRAF_40/CDKN2A+BRAF_H3K27ac_40_Gateway_SuperEnhancers.bed -b ../../macs2/NHM_H3K27ac_40_noTSS.bed| \
-bedtools intersect -a BC_VS_NHM_1pro.bed  -b -|grep Down > NHM_sp_beta.bed
+
+bedtools intersect -b /root/vivek/chip-seq/ROSE/NHM_40/NHM_H3K27ac_40_Gateway_SuperEnhancers.bed \
+-a /root/vivek/chip-seq/macs2/NHM_H3K27ac_40_noTSS.bed| \
+bedtools intersect -b BC_VS_NHM_1pro_down.bed  -a - > NHM_sp_beta.bed
+
+bedtools intersect -b /root/vivek/chip-seq/ROSE/CDKN2A+BRAF_40/CDKN2A+BRAF_H3K27ac_40_Gateway_SuperEnhancers.bed \
+-a /root/vivek/chip-seq/macs2/NHM_H3K27ac_40_noTSS.bed| \
+bedtools intersect -b BC_VS_NHM_1pro_up.bed  -a - > BC_sp_beta.bed
+
+########
+
+bedtools intersect -b /root/vivek/chip-seq/ROSE/NHM_40/NHM_H3K27ac_40_Gateway_SuperEnhancers.bed \
+-a /root/vivek/chip-seq/macs2/NHM_H3K27ac_40_noTSS.bed > NHM_sp_beta.bed
+
+bedtools intersect -b /root/vivek/chip-seq/ROSE/CDKN2A+BRAF_40/CDKN2A+BRAF_H3K27ac_40_Gateway_SuperEnhancers.bed \
+-a /root/vivek/chip-seq/macs2/NHM_H3K27ac_40_noTSS.bed > BC_sp_beta.bed
+
+cat NHM_sp_beta.bed BC_sp_beta.bed | sort -k1,1 -k2,2n| bedtools merge -i - > NHM_BC_sp.bed
