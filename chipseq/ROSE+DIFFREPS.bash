40221c21d08820cebfbfb0a2139718a89e11a2ca
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-18 00:12:41 +0800

    Update ROSE+DIFFREPS.bash

--- chipseq/ROSE+DIFFREPS.bash
+++ chipseq/ROSE+DIFFREPS.bash
@@ -22,3 +22,17 @@ bedtools intersect -b /root/vivek/chip-seq/ROSE/CDKN2A+BRAF_40/CDKN2A+BRAF_H3K27
 -a /root/vivek/chip-seq/macs2/NHM_H3K27ac_40_noTSS.bed > BC_sp_beta.bed
 
 cat NHM_sp_beta.bed BC_sp_beta.bed | sort -k1,1 -k2,2n| bedtools merge -i - > NHM_BC_sp.bed
+################################################################################################################
+################################################################################################################
+################################################################################################################
+################################################################################################################
+
+grep "Down" BC_VS_BRAF_50pro_700w|awk -F"\t" '{if($14<0.05){if($7>50 || $8>50){print $0 }} }'|bedtools intersect \
+-a /root/vivek/chip-seq/macs2/CDKN2A+BRAF_H3K27ac_40.bed -b - |cut -f1,2,3 > BC_VS_BRAF_50pro_700w_DOWN.bed
+grep "Up" BC_VS_BRAF_50pro_700w|awk -F"\t" '{if($14<0.05){if($7>50 || $8>50){print $0 }} }'|bedtools intersect \
+-a /root/vivek/chip-seq/macs2/BRAF_H3K27ac_40.bed -b - |cut -f1,2,3 > BC_VS_BRAF_50pro_700w_UP.bed
+
+grep "Down" BC_VS_NHM_50pro_700w |awk -F"\t" '{if($14<0.05){if($7>50 || $8>50){print $0 }} }'|bedtools intersect \
+-a /root/vivek/chip-seq/macs2/CDKN2A+BRAF_H3K27ac_40.bed -b - |cut -f1,2,3 > BC_VS_NHM_50pro_700w_DOWN.bed
+grep "Up" BC_VS_NHM_50pro_700w |awk -F"\t" '{if($14<0.05){if($7>50 || $8>50){print $0 }} }'|bedtools intersect \
+-a /root/vivek/chip-seq/macs2/NHM_H3K27ac_40.bed -b - |cut -f1,2,3 > BC_VS_NHM_50pro_700w_UP.bed
