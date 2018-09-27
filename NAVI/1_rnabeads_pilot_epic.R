1c9a8e75bcab65a1ef44c448b9038e67da293dc9
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-27 12:10:58 +0200

    Update 1_rnabeads_pilot_epic.R

--- NAVI/1_rnabeads_pilot_epic.R
+++ NAVI/1_rnabeads_pilot_epic.R
@@ -40,3 +40,28 @@ parallel.setup(num.cores)
 
 rnb.run.analysis(dir.reports=report.dir, sample.sheet=sample.annotation, data.dir=idat.dir, data.type="infinium.idat.dir")
 ###############################################################################################################################
+## preprocessing
+#idat files
+idat.dir <- file.path("/home/rtm/vivek/navi/EPIC/idat_Raw_Data")
+
+# Sample annotation
+sample.annotation <- file.path("/home/rtm/vivek/navi/EPIC/rnbeads_sample_sheet.csv")
+rnb.options(import.table.separator=",")
+
+# Report directory
+system("rm -fr /home/rtm/vivek/navi/EPIC/RnBeads/RnBeads_normalization")
+report.dir <- file.path("/home/rtm/vivek/navi/EPIC/RnBeads/RnBeads_normalization")
+
+# Vanilla parameters
+rnb.options(identifiers.column="Sample_ID")
+
+# Multiprocess
+num.cores <- 20
+parallel.setup(num.cores)
+
+data.source <- c(idat.dir, sample.annotation)
+result <- rnb.run.import(data.source=data.source,data.type="infinium.idat.dir", dir.reports=report.dir)
+rnb.set.norm <- rnb.execute.normalization(result$rnb.set, method="swan",bgcorr.method="methylumi.noob")
+
+save.rnb.set(rnb.set.norm,path="/home/rtm/vivek/navi/EPIC/RnBeads/RnBeads_normalization/rnb.set.norm.RData")
+###############################################################################################################################
