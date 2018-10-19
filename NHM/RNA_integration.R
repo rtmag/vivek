6ffe7eaa9f1c85711472d753f87503f9882906a1
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-19 17:49:33 +0200

    Update RNA_integration.R

--- NHM/RNA_integration.R
+++ NHM/RNA_integration.R
@@ -1,1 +1,24 @@
 ##
+NHM = readRDS("NHM_counts.rds")
+BAPOMA = readRDS("bapoma_counts.rds")
+LUWM = readRDS("lu_wm_counts.rds")
+
+options(scipen=999)
+library(DESeq2)
+
+countData = cbind(NHM,BAPOMA,LUWM)
+
+dds <- DESeqDataSetFromMatrix(
+       countData = countData,
+       colData = colData,
+       design = ~ cells)
+
+dLRT <- DESeq(dds, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+dLRT_res <- results(dLRT)
+dLRT_res$padj[is.na(dLRT_res$padj)]=1
+
+pdf("Diagnostic_design_pca.pdf")
+plotPCA(dLRT_vsd,ntop=60000,intgroup=c('cells'))
+dev.off()
+####
