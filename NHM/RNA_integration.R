a28b49eacc671519b839a786d6dd80c4a6ce55c3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-19 18:31:37 +0200

    Update RNA_integration.R

--- NHM/RNA_integration.R
+++ NHM/RNA_integration.R
@@ -1,6 +1,6 @@
 ##
 NHM = readRDS("NHM_counts.rds")
-BAPOMA = readRDS("bapoma_counts.rds")
+BAPOMA = readRDS("bapoma_counts_v27.rds")
 LUWM = readRDS("lu_wm_counts.rds")
 
 options(scipen=999)
@@ -8,17 +8,23 @@ library(DESeq2)
 
 countData = cbind(NHM,BAPOMA,LUWM)
 
+design<-data.frame(group=c("BRAF","BRAF","BRAF",
+                "BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A",
+                "CDKN2A","CDKN2A","CDKN2A",
+                "NHM","NHM","NHM",
+              "BAPOMA","BAP_N","BAPOMA","BAP_N","BAPOMA","BAP_N",
+              "BAPOMA","BAP_N","BAPOMA","BAP_N","BAPOMA","BAP_N",
+               "LU","LU","LU","WM","WM","WM") )
+
 dds <- DESeqDataSetFromMatrix(
        countData = countData,
-       colData = colData,
-       design = ~ cells)
+       colData = design,
+       design = ~ group)
 
 dLRT <- DESeq(dds, test="LRT", reduced=~1)
 dLRT_vsd <- varianceStabilizingTransformation(dLRT)
-dLRT_res <- results(dLRT)
-dLRT_res$padj[is.na(dLRT_res$padj)]=1
 
 pdf("Diagnostic_design_pca.pdf")
-plotPCA(dLRT_vsd,ntop=60000,intgroup=c('cells'))
+plotPCA(dLRT_vsd,ntop=60000,intgroup=c('group'))
 dev.off()
 ####
