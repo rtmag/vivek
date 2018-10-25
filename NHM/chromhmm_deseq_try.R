bf0bc8b34580605a21d9b5f8cb6eef74cd2065a8
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-25 20:00:39 +0200

    Update chromhmm_deseq_try.R

--- NHM/chromhmm_deseq_try.R
+++ NHM/chromhmm_deseq_try.R
@@ -42,3 +42,30 @@ dev.off()
 library(qvalue)
 qvalue(dLRT_res$pvalue)
 ################################################################################
+library(Rsubread)
+
+x=read.table('merged.enhancer_filtered.bed',sep="\t",stringsAsFactors=F)
+
+ann = data.frame(GeneID=paste(x[,1],x[,2],x[,3],sep="_!_"),Chr=x[,1],Start=x[,2],End=x[,3],Strand='+')
+
+bam.files <- c('/root/vivek/chip-seq/bam/NHM_H3K27ac_rmdup.bam',
+              '/root/vivek/chip-seq/bam/BRAF_H3K27ac_rmdup.bam',
+              '/root/vivek/chip-seq/bam/CDKN2A_H3K27ac_rmdup.bam',
+              '/root/vivek/chip-seq/bam/CDKN2A+BRAF_H3K27ac_rmdup.bam')
+
+fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=60)
+
+countData=fc_SE$counts
+colnames(countData) = c("NHM","BRAF","CDKN2A","CB")
+
+max.sec = function(x){n <- length(x); return(sort(x,partial=n-1)[n-1]) }
+
+res=apply(countData,1,function(x) max(x)/max.sec(x))
+en_sp=apply(countData,1,which.max)
+en_sp_ch = en_sp[which(res>2)]
+names(which(en_sp_ch==1))
+          
+write.table(gsub("_!_","\t",names(which(en_sp_ch==1)),perl=T),"NHM_signal_filtered.bed",sep="\t",quote=F,row.name=F,col.name=F)
+write.table(gsub("_!_","\t",names(which(en_sp_ch==2)),perl=T),"BRAF_signal_filtered.bed",sep="\t",quote=F,row.name=F,col.name=F)
+write.table(gsub("_!_","\t",names(which(en_sp_ch==3)),perl=T),"CDKN2A_signal_filtered.bed",sep="\t",quote=F,row.name=F,col.name=F)
+write.table(gsub("_!_","\t",names(which(en_sp_ch==4)),perl=T),"CB_signal_filtered.bed",sep="\t",quote=F,row.name=F,col.name=F)
