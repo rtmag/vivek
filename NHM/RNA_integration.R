3422df6fe321a37db907345563c82440fcb56d77
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-06 14:22:31 +0100

    Update RNA_integration.R

--- NHM/RNA_integration.R
+++ NHM/RNA_integration.R
@@ -1,10 +1,14 @@
+options(scipen=999)
+library(DESeq2)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
 ##
+
 NHM = readRDS("NHM_counts.rds")
 BAPOMA = readRDS("bapoma_counts_v27.rds")
 LUWM = readRDS("lu_wm_counts.rds")
-
-options(scipen=999)
-library(DESeq2)
 
 countData = cbind(NHM,BAPOMA,LUWM)
 
@@ -29,8 +33,6 @@ plotPCA(dLRT_vsd,ntop=60000,intgroup=c('group'))
 dev.off()
 ########################################################################
 ########################################################################
-########################################################################
-########################################################################
 countData = cbind(NHM,BAPOMA,BAPOMA)
 
 colnames(countData) = make.names(colnames(countData),unique=T)
@@ -53,8 +55,70 @@ dds <- DESeqDataSetFromMatrix(
 
 dLRT <- DESeq(dds, test="LRT", reduced=~1)
 dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+vsd = assay(dLRT_vsd)
 
 pdf("Diagnostic_design_pca_NHM_MATCHNEVI.pdf")
 plotPCA(dLRT_vsd,ntop=60000,intgroup=c('group'))
 dev.off()
-####
+######################################################################################################################################
+######################################################################################################################################
+
+NHM = readRDS("NHM_counts.rds")
+LUWM = readRDS("lu_wm_counts.rds")
+
+options(scipen=999)
+library(DESeq2)
+
+countData = cbind(NHM,LUWM)
+########################################################################
+design<-data.frame(group=c("BRAF","BRAF","BRAF",
+                "BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A",
+                "CDKN2A","CDKN2A","CDKN2A",
+                "NHM","NHM","NHM",
+               "LU","LU","LU","WM","WM","WM") )
+
+dds <- DESeqDataSetFromMatrix(
+       countData = countData,
+       colData = design,
+       design = ~ group)
+
+dLRT <- DESeq(dds, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+########################################################################
+
+dds = DESeqDataSetFromMatrix(countData = NHM[,c(10:12,1:3)],
+                             colData = data.frame(group = c("NHM","NHM","NHM","BRAF","BRAF","BRAF") ), 
+                             design = ~ group )
+dds = DESeq(dds)
+dds_res = results(dds,contrast=c("group","BRAF","NHM"))
+
+sig_vsd = vsd[which(abs(dds_res$log2FoldChange)>1 & dds_res$padj<0.05),]
+rownames(sig_vsd) = c()
+colors = colorRampPalette(c("green","black","red"))(20)
+
+png("NHM_vs_BRAF.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+  heatmap.2(as.matrix(sig_vsd),col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="NHM vs BRAF",key.title="Gene expression",cexCol=.9,cexRow=.9,labRow = FALSE)
+dev.off()
+########################################################################
+
+dds = DESeqDataSetFromMatrix(countData = NHM[,c(10:12,4:6)],
+                             colData = data.frame(group = c("NHM","NHM","NHM","BRAF","BRAF","BRAF") ), 
+                             design = ~ group )
+dds = DESeq(dds)
+dds_res = results(dds,contrast=c("group","BRAF","NHM"))
+
+sig_vsd = vsd[which(abs(dds_res$log2FoldChange)>1 & dds_res$padj<0.05),]
+rownames(sig_vsd) = c()
+colors = colorRampPalette(c("green","black","red"))(20)
+
+png("NHM_vs_BC.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+  heatmap.2(as.matrix(sig_vsd),col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="NHM vs BRAF+CDKN2A",key.title="Gene expression",cexCol=.9,cexRow=.9,labRow = FALSE)
+dev.off()
+########################################################################
+
