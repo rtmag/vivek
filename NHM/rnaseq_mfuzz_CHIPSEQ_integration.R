4edb51457117e067f80c8c8f759de40236886f45
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-06 17:28:31 +0100

    Update rnaseq_mfuzz_CHIPSEQ_integration.R

--- NHM/rnaseq_mfuzz_CHIPSEQ_integration.R
+++ NHM/rnaseq_mfuzz_CHIPSEQ_integration.R
@@ -6,7 +6,9 @@ library(RColorBrewer)
 vsd = readRDS("../NHM_vsd.rds")
 vsd = vsd[,c(10:12,1:3,7:9,4:6)]
 
-pdf("c1_mfuzz.pdf")
+png("c1_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
 gmt = read.table("../ct_1_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
@@ -15,7 +17,9 @@ colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","
   xlab="", ylab="Cluster1 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c2_mfuzz.pdf")
+png("c2_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
 gmt = read.table("../ct_2_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
@@ -24,7 +28,9 @@ colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","
   xlab="", ylab="Cluster2 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c3_mfuzz.pdf")
+png("c3_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
 gmt = read.table("../ct_3_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
@@ -33,7 +39,9 @@ colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","
   xlab="", ylab="Cluster3 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c4_mfuzz.pdf")
+png("c4_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
 gmt = read.table("../ct_4_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
@@ -42,43 +50,53 @@ colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","
   xlab="", ylab="Cluster4 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c5_mfuzz.pdf")
+png("c5_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
 gmt = read.table("../ct_5_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
   colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
   heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-  xlab="", ylab="Cluster1 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
+  xlab="", ylab="Cluster5 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c1_mfuzz.pdf")
-gmt = read.table("../ct_1_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
+png("c6_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+gmt = read.table("../ct_6_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
   colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
   heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-  xlab="", ylab="Cluster1 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
+  xlab="", ylab="Cluster6 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c1_mfuzz.pdf")
-gmt = read.table("../ct_1_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
+png("c7_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+gmt = read.table("../ct_7_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
   colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
   heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-  xlab="", ylab="Cluster1 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
+  xlab="", ylab="Cluster7 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c1_mfuzz.pdf")
-gmt = read.table("../ct_1_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
+png("c8_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+gmt = read.table("../ct_8_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
   colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
   heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-  xlab="", ylab="Cluster1 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
+  xlab="", ylab="Cluster8 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
 
-pdf("c9_mfuzz.pdf")
+png("c9_mfuzz.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
 gmt = read.table("../ct_9_rnaseq.txt",sep = "\t", head=F,stringsAsFactors=F)
 sig_vsd = vsd[rownames(vsd) %in% gmt[,1],]
 colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A","BRAF+CDKN2A")
@@ -86,3 +104,6 @@ colnames(sig_vsd) = c("NHM","NHM","NHM","BRAF","BRAF","BRAF","CDKN2A","CDKN2A","
   heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
   xlab="", ylab="Cluster9 Genes",key.title="Gene expression",cexCol=.65,Colv=F,dendrogram="row",labRow = FALSE)
 dev.off()
+
+#######
+
