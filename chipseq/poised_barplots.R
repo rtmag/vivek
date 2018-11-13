659965c617e50742884489f2cd96380ee3a6b26c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-13 18:16:25 +0100

    Update poised_barplots.R

--- chipseq/poised_barplots.R
+++ chipseq/poised_barplots.R
@@ -10,13 +10,13 @@ inter = inter[,c(4,9)]
 inter[,2] = gsub("\\_peak.+","",inter[,2],perl=T)
 
 geneID = unique(inter[,1])
-NHM = geneID
+NHM = rep(NA, length(geneID))
 names(NHM) = geneID
-BRAF = geneID
+BRAF = rep(NA, length(geneID))
 names(BRAF) = geneID
-CDKN2A = geneID
+CDKN2A = rep(NA, length(geneID))
 names(CDKN2A) = geneID
-B_C = geneID
+B_C = rep(NA, length(geneID))
 names(B_C) = geneID
 
 for(i in 1:length(geneID)){
@@ -37,10 +37,33 @@ for(i in 1:length(geneID)){
   if( ("CDKN2A+BRAF_H3K4me3"  %in% inter[inter[,1] %in% geneID[i],2]) & ("CDKN2A+BRAF_H3K27me3"  %in% inter[inter[,1] %in% geneID[i],2]) ){ B_C[i] = "poised"}  
  }
 
+x= cbind(NHM,BRAF,CDKN2A,B_C)
+saveRDS(x,"poised_barplot.rds")
 
 
+tab = apply(x,2,table) 
+tab = rbind(tab,c(
+  46068-sum(tab[,1]),46068-sum(tab[,2]),46068-sum(tab[,3]),46068-sum(tab[,4])
+     ) )
 
+colnames(tab) = c("NHM","BRAF","CDKN2A","BRAF+CDKN2A")
+rownames(tab) = c("H3K27me3","H3K4me3","Poised","Neg")
 
+tab = tab[c(2,1,3,4),]
 
+tab_p = tab
+tab_p[,1] = 100*tab[,1]/46068
+tab_p[,2] = 100*tab[,2]/46068
+tab_p[,3] = 100*tab[,3]/46068
+tab_p[,4] = 100*tab[,4]/46068
+
+pdf("poised_barplot.pdf")
+barplot(tab_p, col=c("#ED0A3F","#1560BD","#3AA655","grey"),ylab = "Promoter (%)")
+dev.off()
+
+pdf("poised_barplot_legend.pdf")
+plot.new()
+legend("top",legend=rownames(tab_p),fill=c("#ED0A3F","#1560BD","#3AA655","grey"))
+dev.off()
 
 
