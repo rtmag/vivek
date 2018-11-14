f60a8d761bd4aa967850246e0812062ae3c34743
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-14 01:51:41 +0100

    Update crest_rna.R

--- NHM/crest_rna.R
+++ NHM/crest_rna.R
@@ -2,36 +2,35 @@ library(Rsubread)
 options(scipen=999)
 
 data<-featureCounts(c(
-"BRAF_1_Aligned.sortedByCoord.out.bam",
-"BRAF_2_Aligned.sortedByCoord.out.bam",
-"BRAF_3_Aligned.sortedByCoord.out.bam",
-"BRAF_+_CDKN2A_1_Aligned.sortedByCoord.out.bam",
-"BRAF_+_CDKN2A_2_Aligned.sortedByCoord.out.bam",
-"BRAF_+_CDKN2A_3_Aligned.sortedByCoord.out.bam",
-"CDKN2A_1_Aligned.sortedByCoord.out.bam",
-"CDKN2A_2_Aligned.sortedByCoord.out.bam",
-"CDKN2A_3_Aligned.sortedByCoord.out.bam",
-"NHM_1_Aligned.sortedByCoord.out.bam",
-"NHM_2_Aligned.sortedByCoord.out.bam",
-"NHM_3_Aligned.sortedByCoord.out.bam"),
+"/root/vivek/vivek_new_rna_seq/bam/NHM_1_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/NHM_2_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/NHM_3_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/BRAF_1_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/BRAF_2_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/BRAF_3_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/CDKN2A_1_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/CDKN2A_2_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/CDKN2A_3_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/BRAF_+_CDKN2A_1_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/BRAF_+_CDKN2A_2_Aligned.sortedByCoord.out.bam",
+"/root/vivek/vivek_new_rna_seq/bam/BRAF_+_CDKN2A_3_Aligned.sortedByCoord.out.bam",
+"crest_Aligned.sortedByCoord.out.bam"),
 annot.ext="/root/resources/gencode.v27.annotation.gtf",
 isGTFAnnotationFile=TRUE,
 minMQS=4,
 strandSpecific=0,
-isPairedEnd=TRUE,
-#PE_orientation="rf",
+isPairedEnd=FALSE,
 autosort=TRUE,
-nthreads=40,
+nthreads=55,
 GTF.attrType="gene_name"
 )
 
 dat=data[[1]]
-colnames(dat)=c("BRAF_1","BRAF_2","BRAF_3",
-                "B_C_1","B_C_2","B_C_3",
-                "CDKN2A_1","CDKN2A_2","CDKN2A_3",
-                "NHM_1","NHM_2","NHM_3")
+colnames(dat)=c("NHM_1","NHM_2","NHM_3","BRAF_1","BRAF_2","BRAF_3",
+                 "CDKN2A_1","CDKN2A_2","CDKN2A_3","B_C_1","B_C_2","B_C_3",
+               "CREST")
 
-saveRDS(dat,"NHM_counts.rds")
+saveRDS(dat,"NHM_crest_counts.rds")
 
 ############################################################################################################
 
