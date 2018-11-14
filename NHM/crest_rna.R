9fa45946335c44a308c74e4b9b418afa0f77ac5e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-14 01:26:38 +0100

    Create crest_rna.R

---
+++ NHM/crest_rna.R
@@ -0,0 +1,43 @@
+library(Rsubread)
+options(scipen=999)
+
+data<-featureCounts(c(
+"BRAF_1_Aligned.sortedByCoord.out.bam",
+"BRAF_2_Aligned.sortedByCoord.out.bam",
+"BRAF_3_Aligned.sortedByCoord.out.bam",
+"BRAF_+_CDKN2A_1_Aligned.sortedByCoord.out.bam",
+"BRAF_+_CDKN2A_2_Aligned.sortedByCoord.out.bam",
+"BRAF_+_CDKN2A_3_Aligned.sortedByCoord.out.bam",
+"CDKN2A_1_Aligned.sortedByCoord.out.bam",
+"CDKN2A_2_Aligned.sortedByCoord.out.bam",
+"CDKN2A_3_Aligned.sortedByCoord.out.bam",
+"NHM_1_Aligned.sortedByCoord.out.bam",
+"NHM_2_Aligned.sortedByCoord.out.bam",
+"NHM_3_Aligned.sortedByCoord.out.bam"),
+annot.ext="/root/resources/gencode.v27.annotation.gtf",
+isGTFAnnotationFile=TRUE,
+minMQS=4,
+strandSpecific=0,
+isPairedEnd=TRUE,
+#PE_orientation="rf",
+autosort=TRUE,
+nthreads=40,
+GTF.attrType="gene_name"
+)
+
+dat=data[[1]]
+colnames(dat)=c("BRAF_1","BRAF_2","BRAF_3",
+                "B_C_1","B_C_2","B_C_3",
+                "CDKN2A_1","CDKN2A_2","CDKN2A_3",
+                "NHM_1","NHM_2","NHM_3")
+
+saveRDS(dat,"NHM_counts.rds")
+
+############################################################################################################
+
+countData=readRDS("NHM_counts.rds")
+options(scipen=999)
+library(DESeq2)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
