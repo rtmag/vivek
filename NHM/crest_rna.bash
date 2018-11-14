48f1a12cd36592f53c838cbbebf6b6dc00180fa9
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-14 01:14:53 +0100

    Update crest_rna.bash

--- NHM/crest_rna.bash
+++ NHM/crest_rna.bash
@@ -1,1 +1,7 @@
 ~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files -Z SRR191362 > SRR191362.fastq
+
+STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50/ \
+--runThreadN 55 \
+--readFilesIn SRR191362.fastq \
+--outSAMtype BAM SortedByCoordinate  \
+--outFileNamePrefix crest_
