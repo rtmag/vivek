df2efa21a0e475d56e24bd0b1bfcc57e38ba7273
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-21 16:50:12 +0100

    Update h3K9me3_DBA.bash

--- chipseq/h3K9me3_DBA.bash
+++ chipseq/h3K9me3_DBA.bash
@@ -5,33 +5,33 @@ bamToBed -i bam/CDKN2A_H3K9me3_rmdup.bam > bed/CDKN2A_H3K9me3.bed &
 bamToBed -i bam/NHM_H3K9me3_rmdup.bam > bed/NHM_H3K9me3.bed &
 ############################################################################################################################
 #BC - N
-diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A+BRAF_H3K27ac.bed \
---control /root/vivek/chip-seq/bed/NHM_H3K27ac.bed --window 1000 --nproc 1 \
---meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/h3k27ac_diff/BC_VS_N_50pro_1000w --noanno --nohs &
+diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A+BRAF_H3K9me3.bed \
+--control /root/vivek/chip-seq/bed/NHM_H3K9me3.bed --window 1000 --nproc 1 \
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/H3K9me3_diff/BC_VS_N_H3K9me3_50pro_1000w --noanno --nohs &
 
 #B - N
-diffReps.pl --treatment /root/vivek/chip-seq/bed/BRAF_H3K27ac.bed \
---control /root/vivek/chip-seq/bed/NHM_H3K27ac.bed --window 1000 --nproc 1 \
---meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/h3k27ac_diff/B_VS_N_50pro_1000w --noanno --nohs &
+diffReps.pl --treatment /root/vivek/chip-seq/bed/BRAF_H3K9me3.bed \
+--control /root/vivek/chip-seq/bed/NHM_H3K9me3.bed --window 1000 --nproc 1 \
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/H3K9me3_diff/B_VS_N_H3K9me3_50pro_1000w --noanno --nohs &
 
 #C - N
-diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A_H3K27ac.bed \
---control /root/vivek/chip-seq/bed/NHM_H3K27ac.bed --window 1000 --nproc 1 \
---meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/h3k27ac_diff/C_VS_N_50pro_1000w --noanno --nohs &
+diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A_H3K9me3.bed \
+--control /root/vivek/chip-seq/bed/NHM_H3K9me3.bed --window 1000 --nproc 1 \
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/H3K9me3_diff/C_VS_N_H3K9me3_50pro_1000w --noanno --nohs &
 
 #BC - B
-diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A+BRAF_H3K27ac.bed \
---control /root/vivek/chip-seq/bed/BRAF_H3K27ac.bed --window 1000 --nproc 1 \
---meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/h3k27ac_diff/BC_VS_B_50pro_1000w --noanno --nohs &
+diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A+BRAF_H3K9me3.bed \
+--control /root/vivek/chip-seq/bed/BRAF_H3K9me3.bed --window 1000 --nproc 1 \
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/H3K9me3_diff/BC_VS_B_H3K9me3_50pro_1000w --noanno --nohs &
 
 #BC - C
-diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A+BRAF_H3K27ac.bed \
---control /root/vivek/chip-seq/bed/CDKN2A_H3K27ac.bed --window 1000 --nproc 1 \
---meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/h3k27ac_diff/BC_VS_C_50pro_1000w --noanno --nohs &
+diffReps.pl --treatment /root/vivek/chip-seq/bed/CDKN2A+BRAF_H3K9me3.bed \
+--control /root/vivek/chip-seq/bed/CDKN2A_H3K9me3.bed --window 1000 --nproc 1 \
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/H3K9me3_diff/BC_VS_C_H3K9me3_50pro_1000w --noanno --nohs &
 
 #B - C
-diffReps.pl --treatment /root/vivek/chip-seq/bed/BRAF_H3K27ac.bed \
---control /root/vivek/chip-seq/bed/CDKN2A_H3K27ac.bed --window 1000 --nproc 1 \
---meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/h3k27ac_diff/B_VS_C_50pro_1000w --noanno --nohs &
+diffReps.pl --treatment /root/vivek/chip-seq/bed/BRAF_H3K9me3.bed \
+--control /root/vivek/chip-seq/bed/CDKN2A_H3K9me3.bed --window 1000 --nproc 1 \
+--meth gt --chrlen ~/resources/hg38.chrom.sizes --mode s --report /root/vivek/chip-seq/H3K9me3_diff/B_VS_C_H3K9me3_50pro_1000w --noanno --nohs &
 
 ############################################################################################################################
