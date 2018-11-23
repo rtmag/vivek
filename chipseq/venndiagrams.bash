1a682aeb9664d156a55f29138f256a2919eda63b
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-23 16:29:41 +0100

    Update venndiagrams.bash

--- chipseq/venndiagrams.bash
+++ chipseq/venndiagrams.bash
@@ -39,3 +39,19 @@ cex = 1,lty =2,cat.cex=1, filename = NULL,margin=.1,col=NA)
 pdf("multi_H3K27ac_noTSS.pdf")
 grid.draw(vp)
 dev.off()
+#####################################################################################################################################
+NHM = read.table("NHM_E4_genes.txt",sep="\t",header=F,stringsAsFactors=F)
+NB = read.table("BRAF_E4_genes.txt",sep="\t",header=F,stringsAsFactors=F)
+NC = read.table("CDKN2A_E4_genes.txt",sep="\t",header=F,stringsAsFactors=F)
+NBC = read.table("BRAF+CDKN2A_E4_genes.txt",sep="\t",header=F,stringsAsFactors=F)
+
+library(VennDiagram)
+#formating
+list.multi<-list(NHM=NHM[,1],NB=NB[,1],NC=NC[,1],NBC=NBC[,1])
+vp <- venn.diagram(list.multi,fill=c("red", "green","blue","orange"), alpha = .3, 
+cex = 1,lty =2,cat.cex=1, filename = NULL,margin=.1,col=NA)
+
+#plotting
+pdf("poisedTSS_venn.pdf")
+grid.draw(vp)
+dev.off()
