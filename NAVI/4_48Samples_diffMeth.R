454cf93d0d1107db8cae0e0e91bf72a473c6c182
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-11-30 16:40:58 +0100

    Update 4_48Samples_diffMeth.R

--- NAVI/4_48Samples_diffMeth.R
+++ NAVI/4_48Samples_diffMeth.R
@@ -303,4 +303,6 @@ rnb.set.filtered <- rnb.execute.na.removal(rnb.set.filtered)$dataset
 meth.filtered <-meth (rnb.set.filtered)
 # 2) Keep CpGs that have no difference between Nevi and Melanoma <.25 in at leasst one CpG
 meth.filtered.centered = meth.norm.centered[rownames(meth.norm.centered) %in% rownames(meth.filtered),]
+# Select CpGs with difference of no more than 10% BetaScore in at least one paired patient sample
 meth.filtered.centered[rowSums(meth.filtered.centered >=0 & meth.filtered.centered <=0.05 ) >= 1, ]
+# 
