40690562ed90bcf06fd55ae0b9aa75aee68e7e0e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-01 22:10:06 +0100

    Update 5_DECONVOLUTION.R

--- NAVI/5_DECONVOLUTION.R
+++ NAVI/5_DECONVOLUTION.R
@@ -12,6 +12,14 @@ library(RColorBrewer)
                     "Melanoma","Nevi","Melanoma","Nevi","Melanoma","Nevi","Melanoma","Nevi")
                     
 meth.filtered = readRDS("meth.filtered.rds")
+
+
+meth.filtered.centered = readRDS("meth.filtered.centered.rds")
+# Select CpGs with difference of no more than 10% BetaScore in at least one paired patient sample
+idx = meth.filtered.centered[rowSums(meth.filtered.centered >=0 & meth.filtered.centered <=0.05 ) >= 8, ]
+##########################################################################################
+
+
 nevi = meth.filtered[,tumor=="Nevi"]
 k = 10
 datafile = nevi
@@ -57,15 +65,6 @@ heatmap.2(as.matrix(all.meth.norm),col=colors,scale="none", trace="none",distfun
 labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
 dev.off()
 
-k = 4
-results <- refactor(datafile,k)
-png("heatmap_REFACTOR_K4_NEVI_10KCpG.png",width= 3.25,
-  height= 3.25,units="in",
-  res=1200,pointsize=4)
-all.meth.norm = nevi[rownames(nevi) %in% head(names(results$RankedProbeNames[1,]),10000), ]
-heatmap.2(as.matrix(all.meth.norm),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
-dev.off()
 
 k = 3
 results <- refactor(datafile,k)
@@ -77,9 +76,40 @@ heatmap.2(as.matrix(all.meth.norm),col=colors,scale="none", trace="none",distfun
 labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
 dev.off()
 
+k = 4
+results <- refactor(datafile,k)
+png("heatmap_REFACTOR_K4_NEVI_10KCpG.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+all.meth.norm = nevi[rownames(nevi) %in% head(names(results$RankedProbeNames[1,]),10000), ]
+heatmap.2(as.matrix(all.meth.norm),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
+dev.off()
+
+melanoma = meth.filtered[,tumor=="Melanoma"]
+png("heatmap_REFACTOR_K4_NEVI_projectedMELANOMA_10KCpG.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+all.meth.norm = melanoma[rownames(melanoma) %in% head(names(results$RankedProbeNames[1,]),10000), ]
+heatmap.2(as.matrix(all.meth.norm),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
+dev.off()
+
+png("heatmap_REFACTOR_K4_NEVI_projectedBOTH_NoDiffPatiets_8440CpG.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+all.meth.norm = meth.filtered[rownames(meth.filtered) %in% head(names(results$RankedProbeNames[1,]),8440), ]
+all.meth.norm= all.meth.norm[rownames(all.meth.norm) %in% rownames(idx),]
+heatmap.2(as.matrix(all.meth.norm),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
+dev.off()
+
 ####################################################################################
 library(sva)
+require(Biobase)
 
-edata = exprs(bladderEset)
+eset<-new("ExpressionSet", exprs=data.matrix(nevi))
+
+edata = exprs(eset)
 mod = model.matrix(~as.factor(cancer), data=pheno)
 n.sv = num.sv(edata,mod,method="leek")
