a3d961c7339cfc4ed459639722a0a10443cd2837
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-05 10:22:38 +0800

    Update 5_DECONVOLUTION.R

--- NAVI/5_DECONVOLUTION.R
+++ NAVI/5_DECONVOLUTION.R
@@ -13,19 +13,17 @@ library(RColorBrewer)
                     
 meth.filtered = readRDS("meth.filtered.rds")
 
-
 meth.filtered.centered = readRDS("meth.filtered.centered.rds")
 # Select CpGs with difference of no more than 10% BetaScore in at least one paired patient sample
 idx = meth.filtered.centered[rowSums(meth.filtered.centered >=0 & meth.filtered.centered <=0.05 ) >= 8, ]
 ##########################################################################################
-
-
 nevi = meth.filtered[,tumor=="Nevi"]
-k = 10
 datafile = nevi
-results <- refactor(datafile,k)
-
 colors <- rev(colorRampPalette( (brewer.pal(9, "PuOr")) )(9))
+#
+
+k = 10
+results <- refactor(datafile,k)
 
 png("heatmap_REFACTOR_K10_NEVI_10KCpG.png",width= 3.25,
   height= 3.25,units="in",
@@ -105,22 +103,18 @@ labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
 dev.off()
 
 ####################################################################################
-library(SmartSVA)
+idx = meth.filtered.centered[rowSums(meth.filtered.centered >=0 & meth.filtered.centered <=0.05 ) >= 24, ]
 
-df <- data.frame(pred=1:23)
-## Determine the number of SVs
-Y.r <- t(resid(lm(t(nevi) ~ pred, data=df)))
-n.sv <- EstDimRMT(Y.r, FALSE)$dim + 1
-mod <- model.matrix( ~ pred, df)
-sv.obj <- smartsva.cpp(nevi, mod=mod, mod0=NULL, n.sv=n.sv)
-
-sv.obj$pprob.gam<0.001
-table(sv.obj$pprob.b<0.001)
-
-all.meth.norm = nevi[sv.obj$pprob.gam<0.001, ]
+png("heatmap_REFACTOR_K4_NEVI_projectedBOTH_NoDiffPatiets_8440CpG.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+all.meth.norm = meth.filtered[rownames(meth.filtered) %in% head(names(results$RankedProbeNames[1,]),100000), ]
+all.meth.norm= all.meth.norm[rownames(all.meth.norm) %in% rownames(idx),]
 heatmap.2(as.matrix(all.meth.norm),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl")
+labRow = FALSE,xlab="", ylab=paste(dim(all.meth.norm)[1],"CpGs"),key.title="Methylation lvl")
+dev.off()
 
-###########
-svobj = sva(nevi,mod,mod0=NULL,n.sv=7)
-pValues = f.pvalue(nevi,mod,mod0)
+
+
+####################################################################################
+rowSums(meth.filtered.centered >=0 & meth.filtered.centered <=0.05 ) >= 24
