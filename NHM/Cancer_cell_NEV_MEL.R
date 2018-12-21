c05e0b80873ed77420fd4c2fef2612ef72e1061d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-20 20:14:36 -0600

    Update Cancer_cell_NEV_MEL.R

--- NHM/Cancer_cell_NEV_MEL.R
+++ NHM/Cancer_cell_NEV_MEL.R
@@ -17,3 +17,28 @@ group_name[grep("Nev",as.character(group_name))] = "Nev"
 group_name[grep("Mel",as.character(group_name))] = "Mel"
 
 colnames(expr) = group_name
+
+gene_name=(data[3:dim(data)[1],4])
+gene_name = make.names(gene_name,unique=T)
+rownames(expr) = gene_name
+
+library(ggplot2)
+library(graphics)
+library(scales)
+library(vegan)
+options(scipen=999)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+library(openxlsx)
+library(beeswarm)
+
+TFAP2C = data.frame(cell = group_name, gene=as.numeric(expr[which(rownames(expr)=="TFAP2C"),]) )
+
+pdf("TFAP2C_Cancer_cell_RNA.pdf")
+stripchart(gene ~ cell, vertical = TRUE, data = TFAP2C, jitter = 0.3, 
+           ylab = expression('Log2 normalized by sample expected counts TFAP2C'),
+    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
+boxplot(gene ~ cell,data = TFAP2C,add=TRUE)
+dev.off()
+
