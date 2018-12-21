66a2eb09467e7c9010e8affcc8b59a179b8a7060
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-20 18:00:32 -0600

    Update Cancer_cell_NEV_MEL.R

--- NHM/Cancer_cell_NEV_MEL.R
+++ NHM/Cancer_cell_NEV_MEL.R
@@ -4,4 +4,16 @@ data <- read_excel("hunterRNAseq.xlsx", sheet = 3)
 data = data.frame(data)
 
 expr = data[3:22463,6:47]
-moko = data[2,6:47]
+sample_name = data[2,6:47]
+
+ix = !1:dim(sample_name)[2] %in% 
+c(grep("T21",as.character(sample_name)),grep("JJS13",as.character(sample_name)),grep("JJS17",as.character(sample_name)))
+
+expr = expr[,ix]
+
+group_name = as.character(sample_name[ix])
+colnames(group_name) = NULL
+group_name[grep("Nev",as.character(group_name))] = "Nev"
+group_name[grep("Mel",as.character(group_name))] = "Mel"
+
+colnames(expr) = group_name
