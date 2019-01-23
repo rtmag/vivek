d96acf698a1d92bea91140c3684a7b8404d6f178
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-01-24 04:17:16 +0800

    Update h3K4me3_poisedChange.bash

--- chipseq/h3K4me3_poisedChange.bash
+++ chipseq/h3K4me3_poisedChange.bash
@@ -32,3 +32,45 @@ k27 = read.table("H3K27me3_premed.bed",sep="\t",header=F)
 
 write.table(k4[k4[,4] %in% k27[,4],],"k4_intk27.bed",quote=F,row.names=F,col.names=F,sep="\t")
 write.table(k27[k27[,4] %in% k4[,4],],"k27_intk4.bed",quote=F,row.names=F,col.names=F,sep="\t")
+#################################
+more /root/resources/hg38_tss.bed|grep -w -f gene.names > tss_genes.bed
+
+computeMatrix reference-point \
+-S \
+/root/vivek/chip-seq/bw/NHM_H3K4me3.bw \
+/root/vivek/chip-seq/bw/NHM_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K27me3.bw \
+-R tss_genes.bed --referencePoint center \
+--sortRegions descend -bs 20 -a 5000 -b 5000 -p max -out tss_genes_transition.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Greens Reds Greens Reds \
+-m tss_genes_transition.mat --kmeans 4 --zMin 0 0 0 0 0 0 0 0 --zMax 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.2 \
+--samplesLabel "H3K4me3 NHM" "H3K27me3 NHM" "H3K4me3 CDKN2A+BRAF" "H3K27me3 CDKN2A+BRAF" \
+-out tss_genes_transition.pdf --outFileSortedRegions tss_genes_transition.bed
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Greens Reds Greens Reds \
+-m tss_genes_transition.mat --kmeans 4 --zMin 0 0 0 0 0 0 0 0 --zMax 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.2 \
+--samplesLabel "H3K4me3 NHM" "H3K27me3 NHM" "H3K4me3 CDKN2A+BRAF" "H3K27me3 CDKN2A+BRAF" \
+-out tss_genes_transition_kmeans4.pdf --outFileSortedRegions tss_genes_transition_kmeans4.bed
+#
+####################################################################################################
+
+computeMatrix reference-point \
+-S \
+/root/vivek/chip-seq/bw/NHM_H3K4me3.bw \
+/root/vivek/chip-seq/bw/NHM_H3K27me3.bw \
+/root/vivek/chip-seq/bw/BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/BRAF_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K27me3.bw \
+-R tss_genes.bed --referencePoint center \
+--sortRegions descend -bs 20 -a 5000 -b 5000 -p max -out tss_genes_transition_all.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Greens Reds Greens Reds Greens Reds Greens Reds \
+-m tss_genes_transition_all.mat --kmeans 4 --zMin 0 0 0 0 0 0 0 0 --zMax 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.2 \
+--samplesLabel "H3K4me3 NHM" "H3K27me3 NHM" "H3K4me3 BRAF" "H3K27me3 BRAF" "H3K4me3 CDKN2A" "H3K27me3 CDKN2A" "H3K4me3 CDKN2A+BRAF" "H3K27me3 CDKN2A+BRAF" \
+-out tss_genes_transition_kmeans4_all.pdf --outFileSortedRegions tss_genes_transition_kmeans4_all.bed
+#
