e35fed3fd7a41f7d8a5bce54789ff8dd20d65cf3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-23 19:15:10 +0800

    Update 10_WES_aln_B.bash

--- NAVI/10_WES_aln_B.bash
+++ NAVI/10_WES_aln_B.bash
@@ -1,104 +1,104 @@
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM19\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM19_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM19_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM19 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM1\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM1_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM1_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM1 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM20\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM20_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM20_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM20 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM25\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM25_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM25_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM25 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM26\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM26_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM26_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM26 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM2\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM2_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM2_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM2 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM31\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM31_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM31_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM31 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM32\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM32_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM32_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM32 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM33\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM33_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM33_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM33 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM34\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM34_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM34_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM34 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM3\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM3_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM3_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM3 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM43\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM43_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM43_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM43 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM44\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM44_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM44_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM44 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM7\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM7_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM7_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM7 -
 
 bwa mem -t 23 -T 0 \
 -R '@RG\tID:VM8\tSM:BATCH1' \
-/home/references/broadhg38/bwa/hg38.fasta.gz \
+/home/references/broadhg38/bwa/Homo_sapiens_assembly38.fasta.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM8_R1_val_1.fq.gz \
 /home/rtm/vivek/navi/wes/fastq_trim/VM8_R2_val_2.fq.gz | \
 samtools view -Shb -o /home/rtm/vivek/navi/wes/bam/VM8 -
