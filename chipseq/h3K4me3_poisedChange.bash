7c6dcd0ce9f0a35ec670cb1c71cc43445fd73ad3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-30 02:11:27 +0800

    Update h3K4me3_poisedChange.bash

--- chipseq/h3K4me3_poisedChange.bash
+++ chipseq/h3K4me3_poisedChange.bash
@@ -138,3 +138,23 @@ plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Gre
 -m tss_genes_transition.mat --zMin 0 0 0 0 0 0 0 0 --zMax 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.2 \
 --samplesLabel "H3K4me3 NHM" "H3K27me3 NHM" "H3K4me3 NBC" "H3K27me3 NBC" \
 -out tss_genes_transition_tss_atLeast_1k4k27.pdf 
+
+#####################################################################################################
+# H3K27me3 peaks in VE+KO not bivalent
+bedtools intersect -a CDKN2A+BRAF_H3K27me3_peaks.broadPeak -b CDKN2A+BRAF_H3K4me3_peaks.broadPeak -v | \
+bedtools intersect -a - -b CDKN2A+BRAF_H3K27ac_peaks.broadPeak -v > /root/vivek/chip-seq/macs2/CDKN2A+BRAF_H3K27me3_notBivalent.bed
+
+computeMatrix reference-point \
+-S \
+/root/vivek/chip-seq/bw/NHM_H3K4me3.bw \
+/root/vivek/chip-seq/bw/NHM_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K27me3.bw \
+-R /root/vivek/chip-seq/macs2/CDKN2A+BRAF_H3K27me3_notBivalent.bed --referencePoint center \
+--sortRegions descend -bs 20 -a 5000 -b 5000 -p max -out /root/vivek/chip-seq/heatmap/CDKN2A+BRAF_H3K27me3_notBivalent.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Greens Reds Greens Reds \
+-m /root/vivek/chip-seq/heatmap/CDKN2A+BRAF_H3K27me3_notBivalent.mat \
+--zMin 0 0 0 0 0 0 0 0 --zMax 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.2 \
+--samplesLabel "H3K4me3 NHM" "H3K27me3 NHM" "H3K4me3 NBC" "H3K27me3 NBC" \
+-out /root/vivek/chip-seq/heatmap/CDKN2A+BRAF_H3K27me3_notBivalent.pdf 
