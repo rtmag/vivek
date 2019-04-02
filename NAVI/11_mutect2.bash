77fa7b9253d2f74790516e333da5e6e38c1ce6b8
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-02 15:58:59 +0800

    Update 11_mutect2.bash

--- NAVI/11_mutect2.bash
+++ NAVI/11_mutect2.bash
@@ -4,13 +4,17 @@ java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.
 
 --restrict-alleles-to BIALLELIC
 
+# Allele frequencies only
+
         # Save off the header for later:
-        grep '^#' ${input_vcf} > header &
+        zcat gnomad.exomes.r2.1.1.sites.vcf.bgz| grep '^#' > header &
 
         # Get all lines in the file except the header:
         # Preserve all fields before INFO, Grab only the AF annotation from the INFO Field
         # replace ID (3rd) and QUAL (6th) columns with '.' (empty):
-        grep -v "^#" ${input_vcf} | sed -e 's#\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t.*;AF=\([0-9]*\.[e0-9+-]*\).*#\1\t\2\t.\t\4\t\5\t.\t\7\tAF=\8#g' > simplified_body &
+        zcat gnomad.exomes.r2.1.1.sites.vcf.bgz|grep -v "^#" | \
+        sed -e 's#\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t.*;AF=\([0-9]*\.[e0-9+-]*\).*#\1\t\2\t.\t\4\t\5\t.\t\7\tAF=\8#g' \
+        > simplified_body &
 
         # Wait for background processes to finish:
         wait
@@ -19,13 +23,14 @@ java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.
         cat header simplified_body > simplified.vcf
 
         # Zip the VCF:
-        bgzip simplified.vcf -O ${output_name}.vcf.gz
+        bgzip simplified.vcf -O gnomad.exomes.r2.1.1.AFonly.vcf.gz
 
         # Index output file:
-        gatk --java-options "-Xmx${command_mem}g" IndexFeatureFile -F ${output_name}.vcf.gz
+        java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.jar IndexFeatureFile \
+        -F gnomad.exomes.r2.1.1.AFonly.vcf.gz
 
         # Cleanup:
-        rm -f header body simplified_info simplified_body simplified.vcf simplified.vcf.idx
+        rm header body simplified_info simplified_body simplified.vcf simplified.vcf.idx
 
 ########
 java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.jar CollectSequencingArtifactMetrics \
