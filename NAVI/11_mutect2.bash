e242599eb5fb5a453a337fbf824871e9bab2b89f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-02 16:48:30 +0800

    Update 11_mutect2.bash

--- NAVI/11_mutect2.bash
+++ NAVI/11_mutect2.bash
@@ -36,6 +36,13 @@ java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.
         awk '{ if($0 !~ /^#/) print "chr"$0; else if(match($0,/(##contig=<ID=)(.*)/,m)) print m[1]"chr"m[2]; else print $0 }' \
         no_chr.vcf > with_chr.vcf
 
+ java -jar picard.jar LiftoverVcf \\
+     I=input.vcf \\
+     O=lifted_over.vcf \\
+     CHAIN=b37tohg38.chain \\
+     REJECT=rejected_variants.vcf \\
+     R=reference_sequence.fasta
+
 ########
 java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.jar CollectSequencingArtifactMetrics \
 -I /home/rtm/vivek/navi/wes/bam/VM8_recalibrated.bam \
