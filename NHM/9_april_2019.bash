19647b3efd7dbf8ed1f213d11f80fa1a3fe452eb
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-12 16:47:29 +0800

    Create 9_april_2019.bash

---
+++ NHM/9_april_2019.bash
@@ -0,0 +1,15 @@
+# !) ADD H1-hesc heat map to the bivalent heat map (change color slightly and futon the right side) - withOur pipeline
+# 3) take our bivalent regions and plot berstein h1hesc data h3k4me3 and h3k27me
+
+#H3k4me3 ENCSR019SQX
+# 36 nt se
+wget https://www.encodeproject.org/files/ENCFF296CUY/@@download/ENCFF296CUY.fastq.gz
+# 76 nt se
+wget https://www.encodeproject.org/files/ENCFF739WMB/@@download/ENCFF739WMB.fastq.gz
+
+# h3k27me3 ENCSR216OGD
+# SE 76nt
+wget https://www.encodeproject.org/files/ENCFF539PKS/@@download/ENCFF539PKS.fastq.gz
+# SE 76nt
+wget https://www.encodeproject.org/files/ENCFF374VEK/@@download/ENCFF374VEK.fastq.gz
+###############################################################################################
