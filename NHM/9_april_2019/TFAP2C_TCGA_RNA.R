bd51c8b4f9d4a5870277881485a919a75fb186eb
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-12 23:39:36 +0800

    Update TFAP2C_TCGA_RNA.R

--- NHM/9_april_2019/TFAP2C_TCGA_RNA.R
+++ NHM/9_april_2019/TFAP2C_TCGA_RNA.R
@@ -9,20 +9,18 @@ data1 = data[which(data$Hugo_Symbol %in% c("TFAP2A","TFAP2B","TFAP2C","TFAP2D","
                                             "BAP1","SF3B1","EIF1AX","GAPDH","ACTB","HSPA4","HSPA5")),]
 rownames(data1) = data1[,1]
 data1 = data1[,3:dim(data1)[2]]
-data1 = log2(t(data1+1))
+data1 = log2((data1+1))
+jit1 <- setNames(split(data1, seq(nrow(data1))), rownames(data1))
+box1 <- t(data1)
+#####################################################################################################################
 
-boxplot(data1)
+#pdf("beeswarm_test.pdf")
+#beeswarm(data1,pch = 16)
+#dev.off()
 
-
-stripchart(gene ~ cell, vertical = TRUE, data = H2AFV, jitter = 0.3, ylab = expression('Single Cell RNA-Seq H2AFV'),
-    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
-
-test = list(TFAP2A=data1[,1],TFAP2B=data1[,2])
-test = list(data1)
-names(test) = rownames(data1)
-
-beeswarm(test)
-stripchart(test,vertical = TRUE,method = "jitter", pch = 20)
-boxplot(test, add = TRUE)
-
+pdf("jitter_SKCM_RNA.pdf")
+stripchart(jit1,vertical = TRUE,jitter = 0.3, ylab = expression('Log'[2]*' Normalized Tag Count TCGA:SKCM'),
+           method = "jitter", pch = 20, col = alpha(colour='red',alpha=.1),cex = 2,las=2)
+boxplot(box1, add = TRUE,boxlwd = 2,las=2)
+dev.off()
 
