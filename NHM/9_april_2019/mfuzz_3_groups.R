6102cb150dcc5258a6c7851e2bdfd9609778fa55
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-12 23:56:50 +0800

    Update mfuzz_3_groups.R

--- NHM/9_april_2019/mfuzz_3_groups.R
+++ NHM/9_april_2019/mfuzz_3_groups.R
@@ -1,3 +1,14 @@
+options(scipen=999)
+library(DESeq2)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
+require(Biobase)
+library(Mfuzz)
+library(ggplot2)
+options(bitmapType="cairo")
+
 # no CDKN2A
 countData=readRDS("NHM_counts.rds")
 countData = countData[,c(10:12,1:6)]
@@ -14,16 +25,14 @@ sig_vsd = data.frame(NHM=rowMeans(vsd[which(dLRT_res$padj<0.00001),1:3]),BRAF=ro
 
 wt<-new("ExpressionSet", exprs=as.matrix(sig_vsd))
 wt.s<-standardise(wt)
-cl_wt<-mfuzz(wt.s,c=5,m=mestimate(wt.s))
-saveRDS(cl_wt,"cl_wt_NHM_VE_VEKO_RNASEQ_mfuzz_FDR1e.rds")
-pdf('NHM_VE_VEKO_RNASEQ_mfuzz_FDR1e-5.pdf')
-mfuzz.plot(wt.s,cl=cl_wt,mfrow=c(3,2),new.window=F,time.labels=c("NHM","VE","VE+KO"))
+cl_wt<-mfuzz(wt.s,c=3,m=mestimate(wt.s))
+mfuzz.plot(wt.s,cl=cl_wt,mfrow=c(3,1),new.window=F,time.labels=c("NHM","VE","VE+KO"))
+saveRDS(cl_wt,"cl_wt_NHM_VE_VEKO_RNASEQ_mfuzz_FDR1e-5_K3.rds")
+pdf('NHM_VE_VEKO_RNASEQ_mfuzz_FDR1e-5_K3.pdf',width=2.3,)
+mfuzz.plot(wt.s,cl=cl_wt,mfrow=c(3,1),new.window=F,time.labels=c("NHM","VE","VE+KO"))
 dev.off()
 table(cl_wt$cluster)
 
 write.table(names(cl_wt$cluster[cl_wt$cluster==1]) ,"NHM_VE_VEKO_RNASEQ_mfuzz_ct_1_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
 write.table(names(cl_wt$cluster[cl_wt$cluster==2]) ,"NHM_VE_VEKO_RNASEQ_mfuzz_ct_2_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
 write.table(names(cl_wt$cluster[cl_wt$cluster==3]) ,"NHM_VE_VEKO_RNASEQ_mfuzz_ct_3_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
-write.table(names(cl_wt$cluster[cl_wt$cluster==4]) ,"NHM_VE_VEKO_RNASEQ_mfuzz_ct_4_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
-write.table(names(cl_wt$cluster[cl_wt$cluster==5]) ,"NHM_VE_VEKO_RNASEQ_mfuzz_ct_5_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
-write.table(names(cl_wt$cluster[cl_wt$cluster==6]) ,"NHM_VE_VEKO_RNASEQ_mfuzz_ct_6_rnaseq.txt",quote=F,col.names=F,row.names=F,sep="\t")
