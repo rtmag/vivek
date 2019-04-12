f8a78d96108492e014e3ebae24e264243469ace6
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-12 17:41:26 +0800

    Update 9_april_2019.bash

--- NHM/9_april_2019.bash
+++ NHM/9_april_2019.bash
@@ -13,3 +13,41 @@ wget https://www.encodeproject.org/files/ENCFF539PKS/@@download/ENCFF539PKS.fast
 # SE 76nt
 wget https://www.encodeproject.org/files/ENCFF374VEK/@@download/ENCFF374VEK.fastq.gz
 ###############################################################################################
+
+STAR --genomeDir /root/resources/hg38_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn ENCFF296CUY.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix H3K4me3_36_
+
+
+STAR --genomeDir /root/resources/hg38_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn ENCFF296CUY.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix H3K4me3_76_
+
+STAR --genomeDir /root/resources/hg38_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn ENCFF539PKS.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix H3K27me3_76_
+
+STAR --genomeDir /root/resources/hg38_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn ENCFF374VEK.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix H3K27me3_36_
+#####################
