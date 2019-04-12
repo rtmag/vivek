8abb28b0a9dd7ce81f90d5c6d5a9c63ba1275f9f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-13 00:43:01 +0800

    Update encode_heatmaps.bash

--- NHM/9_april_2019/encode_heatmaps.bash
+++ NHM/9_april_2019/encode_heatmaps.bash
@@ -103,4 +103,52 @@ bamCoverage -p max -e 200 -bs 1 --normalizeUsing CPM -b ENCFF083QQZ.bam -o dw.H3
 bamCoverage -p max -e 200 -bs 1 --normalizeUsing CPM -b ENCFF310SBN.bam -o dw.H3K27me3_2_rmdup.bw
 bamCoverage -p max -e 200 -bs 1 --normalizeUsing CPM -b ENCFF467XCU.bam -o dw.H3K4me3_1_rmdup.bw
 bamCoverage -p max -e 200 -bs 1 --normalizeUsing CPM -b ENCFF494FNC.bam -o dw.H3K4me3_2_rmdup.bw
+##################################################################################################################
+computeMatrix reference-point \
+-S \
+/root/vivek/chip-seq/bw/NHM_H3K4me3.bw \
+/root/vivek/chip-seq/bw/NHM_H3K27me3.bw \
+/root/vivek/chip-seq/bw/BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/BRAF_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K27me3.bw \
+/root/vivek/chip-seq/roadmap/dw.H3K4me3_1_rmdup.bw \
+/root/vivek/chip-seq/roadmap/dw.H3K4me3_2_rmdup.bw \
+/root/vivek/chip-seq/roadmap/dw.H3K27me3_1_rmdup.bw \
+/root/vivek/chip-seq/roadmap/dw.H3K27me3_2_rmdup.bw \
+-R /root/vivek/NHM/joe_costello/H1_heSC_poised_TSS.bed --referencePoint center \
+--sortRegions descend -bs 20 -a 5000 -b 5000 -p max -out H1_heSC_poised_withRoadmapSample.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "H1-PoisedTSS" \
+--colorMap Greens Reds Greens Reds Greens Reds Greens Reds Purples Purples Oranges Oranges \
+-m H1_heSC_poised_withRoadmapSample.mat --zMin 0 0 0 0 0 0 0 0 --zMax 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.7 1.2 1.2 \
+--samplesLabel "H3K4me3 NHM" "H3K27me3 NHM" "H3K4me3 NB" "H3K27me3 NB" "H3K4me3 NC" "H3K27me3 NC" "H3K4me3 NBC" "H3K27me3 NBC" \
+"H3K4me3 hESC" "H3K4me3 hESC" "H3K27me3 hESC" "H3K27me3 hESC" \
+-out H1_heSC_poised_withRoadmapSample_dw.pdf
+
+computeMatrix reference-point \
+-S \
+/root/vivek/chip-seq/bw/NHM_H3K4me3.bw \
+/root/vivek/chip-seq/bw/NHM_H3K27me3.bw \
+/root/vivek/chip-seq/bw/BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/BRAF_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A_H3K27me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K4me3.bw \
+/root/vivek/chip-seq/bw/CDKN2A+BRAF_H3K27me3.bw \
+/root/vivek/chip-seq/roadmap/H3K4me3_36_rmdup.bw \
+/root/vivek/chip-seq/roadmap/H3K4me3_76_rmdup.bw \
+/root/vivek/chip-seq/roadmap/H3K27me3_36_rmdup.bw \
+/root/vivek/chip-seq/roadmap/H3K27me3_76_rmdup.bw \
+-R /root/vivek/NHM/joe_costello/H1_heSC_poised_TSS.bed --referencePoint center \
+--sortRegions descend -bs 20 -a 5000 -b 5000 -p max -out H1_heSC_poised_withRoadmapSample.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "H1-PoisedTSS" \
+--colorMap Greens Reds Greens Reds Greens Reds Greens Reds Purples Purples Oranges Oranges \
+-m H1_heSC_poised_withRoadmapSample.mat --zMin 0 0 0 0 0 0 0 0 --zMax 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.2 1.7 1.7 1.2 1.2 \
+--samplesLabel "H3K4me3 NHM" "H3K27me3 NHM" "H3K4me3 NB" "H3K27me3 NB" "H3K4me3 NC" "H3K27me3 NC" "H3K4me3 NBC" "H3K27me3 NBC" \
+"H3K4me3 hESC" "H3K4me3 hESC" "H3K27me3 hESC" "H3K27me3 hESC" \
+-out H1_heSC_poised_withRoadmapSample_STAR.pdf
 ########
