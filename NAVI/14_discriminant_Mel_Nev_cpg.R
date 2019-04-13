94241edb35a0c66fd2ef948b8633fea561146734
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-13 21:08:35 +0800

    Update 14_discriminant_Mel_Nev_cpg.R

--- NAVI/14_discriminant_Mel_Nev_cpg.R
+++ NAVI/14_discriminant_Mel_Nev_cpg.R
@@ -1,8 +1,41 @@
+############################################################################################
+suppressMessages(library(RnBeads))
+options(bitmapType="cairo")
+options(scipen=999)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+############################################################################################
+rnb.set.filtered=load.rnb.set("/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_normalization/rnb.set.norm.filtered.RData.zip")
+meth.norm<-meth(rnb.set.filtered,row.names=T)
+mval.norm <- mval(rnb.set.filtered,row.names=T)
+dmc_table <- readRDS("dmc_MvsN_byPatient.rds")
 
-# Removing Patient pairing
-rnb.options("columns.pairing"=NULL)
-MvsN_dmc_noPatient <- rnb.execute.computeDiffMeth(rnb.set.norm_no910,pheno.cols=c("Tumor"))
+track=as.character(rnb.set.filtered@pheno$Tumor)
+track[track=="Melanoma"]=1
+track[track=="Nevi"]=2
+track[track=="MIS"]=3
 
-comparison <- get.comparisons(MvsN_dmc_noPatient)[1]
-dmc_noPatient_table <-get.table(MvsN_dmc_noPatient, comparison, "sites", return.data.frame=TRUE)
-saveRDS(dmc_noPatient_table,"dmc_MvsN_NoPatient.rds")
+track=as.numeric(track)
+colores=c("#ffb3ba","#baffc9","#bae1ff")
+clab=as.character(colores[track])
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(9))
+
+
+meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.05 & abs(dmc_table$mean.diff)>.25 ,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.05 & abs(dmc_table$mean.quot.log2)>1.5 ,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+
+png("heatmap_FDR5e-3_DIF80_no9_no10_Nocentered.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+#######################################################################################
