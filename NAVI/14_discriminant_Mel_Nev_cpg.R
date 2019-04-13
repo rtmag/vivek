ddfb5fcdc47ee55a45f8e5b008e4c0e6152c4fcb
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-13 22:07:21 +0800

    Update 14_discriminant_Mel_Nev_cpg.R

--- NAVI/14_discriminant_Mel_Nev_cpg.R
+++ NAVI/14_discriminant_Mel_Nev_cpg.R
@@ -6,10 +6,113 @@ library(gplots)
 library(factoextra)
 library(RColorBrewer)
 ############################################################################################
+############################################################################################
+# Paired Analysis
 rnb.set.filtered=load.rnb.set("/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_normalization/rnb.set.norm.filtered.RData.zip")
 meth.norm<-meth(rnb.set.filtered,row.names=T)
 mval.norm <- mval(rnb.set.filtered,row.names=T)
 dmc_table <- readRDS("dmc_MvsN_byPatient.rds")
+
+track=as.character(rnb.set.filtered@pheno$Tumor)
+track[track=="Melanoma"]=1
+track[track=="Nevi"]=2
+track[track=="MIS"]=3
+
+track=as.numeric(track)
+colores=c("#ffb3ba","#baffc9","#bae1ff")
+clab=as.character(colores[track])
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(9))
+
+# standard
+meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.01 & abs(dmc_table$mean.diff)>.28 ,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+png("heatmap_FDR5e-3_DIF25_no9_no10_NotCentered.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+
+# quot
+meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.05 & abs(dmc_table$mean.quot.log2)>1.5 ,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+png("heatmap_FDR5e-3_Quot1.5_no9_no10_NotCentered.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+
+# combrank
+meth.norm.sig=meth.norm[ order(dmc_table$combinedRank) ,]
+meth.norm.sig = meth.norm.sig[1:100,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+png("heatmap_CombRank100_no9_no10_NotCentered.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+
+# FDR5e-5_dif25
+meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.00005 & abs(dmc_table$mean.diff)>.25 ,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+png("heatmap_FDR5e-5_dif25_no9_no10_NotCentered.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+
+# FDR5e-4_dif25
+meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.0005 & abs(dmc_table$mean.diff)>.25 ,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+png("heatmap_FDR5e-4_dif25_no9_no10_NotCentered.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+
+# 40 CPG
+rnb.set.norm=load.rnb.set("/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_normalization/rnb.set.norm.RData.zip")
+x = read.table("/home/rtm/vivek/navi/meth_GEO/40_cpg_elasticNet.txt")
+meth.norm_or<-meth(rnb.set.norm,row.names=T)
+meth.norm.sig=meth.norm_or[ rownames(meth.norm_or) %in% as.character(x[,1]) ,]
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+dim(meth.norm.sig)
+
+png("heatmap_40_CpG.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+
+############################################################################################
+############################################################################################
+# Paired Analysis
+
+rnb.set.filtered=load.rnb.set("/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_normalization/rnb.set.norm.filtered.RData.zip")
+meth.norm<-meth(rnb.set.filtered,row.names=T)
+mval.norm <- mval(rnb.set.filtered,row.names=T)
+dmc_table <- readRDS("dmc_MvsN_NoPatient.rds")
 
 track=as.character(rnb.set.filtered@pheno$Tumor)
 track[track=="Melanoma"]=1
@@ -26,16 +129,32 @@ meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.05 & abs(dmc_table$mean.
 meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
 dim(meth.norm.sig)
 
+png("heatmap_FDR5e-3_DIF25_no9_no10_NotCentered_notPaired_NoPatient.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+
 meth.norm.sig=meth.norm[ dmc_table$diffmeth.p.adj.fdr<0.05 & abs(dmc_table$mean.quot.log2)>1.5 ,]
 meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
 dim(meth.norm.sig)
 
-
-png("heatmap_FDR5e-3_DIF80_no9_no10_Nocentered.png",width= 3.25,
+png("heatmap_FDR5e-3_Quot1.5_no9_no10_NotCentered_notPaired_NoPatient.png",width= 3.25,
   height= 3.25,units="in",
   res=1200,pointsize=4)
 x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl",ColSideColors=clab)
+labRow = FALSE,xlab="", ylab=paste(dim(meth.norm.sig)[1],"CpGs"),key.title="Methylation lvl",ColSideColors=clab)
 legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
 dev.off()
-#######################################################################################
+
+
+############################################################################################
+############################################################################################
+
+GSE86355 = read.table("GSE86355_unmethylated_methylated_signal.txt.gz")
+GSE120878 = read.table("GSE120878_Matrix_Intensity_Pval.txt.gz")
+
+GSE86355_anno = read.table("GSE86355_annotation.txt")
+GSE120878_anno = read.table("GSE120878_annotation.txt")
