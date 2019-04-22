3cbe241945ef39157b0e8329680913ea0e0536f3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-22 22:31:34 +0800

    Update TCGA_meth_on_bivalent.R

--- NHM/22_april_2019/TCGA_meth_on_bivalent.R
+++ NHM/22_april_2019/TCGA_meth_on_bivalent.R
@@ -1,7 +1,18 @@
 # get bw meth
+
+info = read.table("hm450.hg38.manifest.tsv",sep="\t", header=T)
+
 x = readRDS("/root/TCGA/Rnbeads/SKCM/RnBeads_normalization/betaVALUES.rds")
-x1 = readRDS("/root/TCGA/Rnbeads/SKCM/RnBeads_normalization/betaVALUES_withNormal.rds")
-
-info = read.csv("/root/TCGA/Rnbeads/SKCM/SKCM_TP53_mutation_info_withNormal.csv")
-
 meanx = rowMeans(x)
+
+info = info[,c(1,2,3,5)]
+
+ix = match(info[,4], names(meanx) )
+
+table( (info[,4]) == (names(meanx[ix])) )
+
+bg = cbind(info[,1:3],meanx[ix])
+colnames(bg) = NULL
+rownames(bg) = NULL
+
+write.table(bg,"SKCM_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
