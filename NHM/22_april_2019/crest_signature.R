1e49e1f052d7709efe4d501b05ddee962797ee87
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-29 23:14:58 +0800

    Update crest_signature.R

--- NHM/22_april_2019/crest_signature.R
+++ NHM/22_april_2019/crest_signature.R
@@ -8,7 +8,7 @@ library(RColorBrewer)
   colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(9))
 
 
-vsd = readRDS("../crest_vsd.rds")
+vsd = readRDS("crest_vsd.rds")
 
 colnames(vsd) = c("NHM_1","NHM_2","NHM_3",
                  "VE_1","VE_2","VE_3",
@@ -66,4 +66,141 @@ x = x[apply(x,1,sd)!=0,]
 pdf("NCC_transitory_SIGNATURE_NHM.pdf",height=10,width=3.5)
   heatmap.2(x,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
   xlab="", ylab="Neural crest-like-Transitory Gene Signature",key.title="Gene expression",cexCol=.85,cexRow=.6,Colv = "none",dendrogram="row")
+dev.off()
+################################################################################################
+sig_3g = sig[sig$Signature %in% c("Melanocytic","Neural crest-like","Undifferentiated"),1:2]
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig_3g[,1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+
+track=as.character(rownames(x))
+track[track %in% sig$Gene[sig$Signature=="Melanocytic"] ]=1
+track[track %in% sig$Gene[sig$Signature=="Neural crest-like"] ]=2
+track[track %in% sig$Gene[sig$Signature=="Undifferentiated"] ]=3
+track=as.numeric(track)
+colores=c("#ffb3ba","#baffc9","#bae1ff")
+rlab=as.character(colores[track])
+
+pdf("combined_SIGNATURE_NHM.pdf",height=10,width=3.5)
+ x=heatmap.2(x,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="Genes",key.title="Gene expression",cexCol=.85,cexRow=.6,
+           Colv = "none",dendrogram="row",RowSideColors=rlab)
+legend("topright",legend=c("Melanocytic","Neural crest-like","Undifferentiated"),
+       fill=c("#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
+################################################################################################
+# CREST 
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig[sig$Signature=="Neural crest-like",1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+pdf("CREST_SIGNATURE_NHM.pdf",height=10,width=3.5)
+  heatmap.2(x,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="CREST Gene Signature",key.title="Gene expression",cexCol=.85,cexRow=.6,Colv = "none",dendrogram="row")
+dev.off()
+################################################################################################
+# Melanocytic 
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig[sig$Signature=="Melanocytic",1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+pdf("Melanocytic_SIGNATURE_NHM.pdf",height=10,width=3.5)
+  heatmap.2(x,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="Melanocytic Gene Signature",key.title="Gene expression",cexCol=.85,cexRow=.6,Colv = "none",dendrogram="row")
+dev.off()
+################################################################################################
+# Undifferentiated 
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig[sig$Signature=="Undifferentiated",1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+pdf("Undifferentiated_SIGNATURE_NHM.pdf",height=10,width=3.5)
+  heatmap.2(x,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+  xlab="", ylab="Undifferentiated Gene Signature",key.title="Gene expression",cexCol=.85,cexRow=.6,Colv = "none",dendrogram="row")
+dev.off()
+################################################################################################
+sig_3g = sig[sig$Signature %in% c("Melanocytic","Neural crest-like","Undifferentiated"),1:2]
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig_3g[,1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+rownormx= t(apply(x, 1, function(x)(x-min(x))/(max(x)-min(x))))
+rownormx=cbind(rownormx,Neural_Crest=0)
+rownormx=cbind(rownormx,Melanocytic=0)
+rownormx=cbind(rownormx,Undifferentiated=0)
+
+rownormx[rownames(rownormx) %in% sig$Gene[sig$Signature=="Neural_Crest"], "Neural_Crest" ]=1
+rownormx[rownames(rownormx) %in% sig$Gene[sig$Signature=="Melanocytic"], "Melanocytic" ]=1
+rownormx[rownames(rownormx) %in% sig$Gene[sig$Signature=="Undifferentiated"], "Undifferentiated" ]=1
+
+sampleDists <- get_dist(t(rownormx),method="euclidean")
+sampleDistMatrix <- as.matrix(sampleDists)
+                  
+library(pheatmap)
+colors <- (colorRampPalette( (brewer.pal(9, "RdYlBu")) )(200))
+pdf("combined_sampleDistance.pdf")
+                  pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
+dev.off()
+####################################################################################
+                  
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig[sig$Signature=="Neural crest-like",1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+
+rownormx= t(apply(x, 1, function(x)(x-min(x))/(max(x)-min(x))))
+rownormx=cbind(rownormx,Neural_Crest=1)
+sampleDists <- get_dist(t(rownormx),method="euclidean")
+sampleDistMatrix <- as.matrix(sampleDists)
+                  
+library(pheatmap)
+colors <- (colorRampPalette( (brewer.pal(9, "RdYlBu")) )(200))
+pdf("NCC_sampleDistance.pdf")
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
+dev.off()
+
+                  
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig[sig$Signature=="Melanocytic",1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+                  
+rownormx= t(apply(x, 1, function(x)(x-min(x))/(max(x)-min(x))))
+rownormx=cbind(rownormx,Melanocytic=1)
+sampleDists <- get_dist(t(rownormx),method="euclidean")
+sampleDistMatrix <- as.matrix(sampleDists)
+                  
+library(pheatmap)
+colors <- (colorRampPalette( (brewer.pal(9, "RdYlBu")) )(200))
+pdf("Melanocytic_sampleDistance.pdf")
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
+dev.off()
+
+sig_vsd = vsd[rownames(vsd) %in% as.character(sig[sig$Signature=="Undifferentiated",1]),]
+sig_vsd = sig_vsd[apply(sig_vsd,1,sd)!=0,]
+x = sig_vsd[,c(1:6,10:12)]
+x = x[apply(x,1,sd)!=0,]
+                  
+rownormx= t(apply(x, 1, function(x)(x-min(x))/(max(x)-min(x))))
+rownormx=cbind(rownormx,Undifferentiated=1)
+sampleDists <- get_dist(t(rownormx),method="euclidean")
+sampleDistMatrix <- as.matrix(sampleDists)
+                  
+library(pheatmap)
+colors <- (colorRampPalette( (brewer.pal(9, "RdYlBu")) )(200))
+pdf("Undifferentiated_sampleDistance.pdf")
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
 dev.off()
