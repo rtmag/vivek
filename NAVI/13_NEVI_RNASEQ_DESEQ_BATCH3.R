d3ea5156f219387dc312e086db08bbb5a300a9ab
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-01 14:03:50 +0800

    Update 13_NEVI_RNASEQ_DESEQ_BATCH3.R

--- NAVI/13_NEVI_RNASEQ_DESEQ_BATCH3.R
+++ NAVI/13_NEVI_RNASEQ_DESEQ_BATCH3.R
@@ -140,7 +140,7 @@ design$Batch = as.factor(design$Batch)
 library(gtools)
 design = design[mixedorder(design$VM_ID),]
 #paired grouping
- Patient= c(0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,0,0,0,0,0,0,0,0,0)
+ Patient= c(0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,0,19,19,20,20,21,21,22,22)
 design$Patient = Patient
 design$Patient =  as.factor(design$Patient)
 design = design[match(colnames(countData),design$RNA_ID),]
