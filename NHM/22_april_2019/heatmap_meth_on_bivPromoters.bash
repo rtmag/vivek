40157401ba90b306fda44a269642de821a566cd2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-20 02:24:50 +0800

    Update heatmap_meth_on_bivPromoters.bash

--- NHM/22_april_2019/heatmap_meth_on_bivPromoters.bash
+++ NHM/22_april_2019/heatmap_meth_on_bivPromoters.bash
@@ -1,6 +1,7 @@
 grep -v "NA" SKCM_mean_beta.bedgraph > SKCM_mean_beta_withoutNA.bedgraph
 awk -F"\t" '{print $1"\t"$3-1"\t"$3"\t"$4}' SKCM_mean_beta_withoutNA.bedgraph > SKCM_mean_beta_withoutNA_unmerged.bedgraph
 
+#https://www.biostars.org/p/164020/
 bedops --partition SKCM_mean_beta_withoutNA_unmerged.bedgraph | \
 bedmap --echo --echo-map-id-uniq --delim '\t' - SKCM_mean_beta_withoutNA_unmerged.bedgraph | \
 awk '{ n = split($4, a, ";"); max = a[1]; for(i = 2; i <= n; i++) { if (a[i] > max) { max = a[i];} } print $1"\t"$2"\t"$3"\t"max; }' - \
@@ -32,3 +33,33 @@ plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "PoisedTSS" \
 -m SKCM_mean_beta_on_poised_kmeans_zmet_5k_3clusters.mat \
 --samplesLabel "SKCM AVG DNA methylation" \
 -out SKCM_mean_beta_on_poised_kmeans_zmet_5k_3clusters.pdf
+
+####################################################################################################################################
+~/myprograms/kentUtils/bin/linux.x86_64/bedGraphToBigWig melanoma_prev450k_mean_beta.bedgraph /home/rtm/resources/hg38.chrom.sizes \
+melanoma_prev450k_mean_beta.bw
+
+~/myprograms/kentUtils/bin/linux.x86_64/bedGraphToBigWig melanoma_train_mean_beta.bedgraph /home/rtm/resources/hg38.chrom.sizes \
+melanoma_train_mean_beta.bw
+
+~/myprograms/kentUtils/bin/linux.x86_64/bedGraphToBigWig nevi_prev450k_mean_beta.bedgraph /home/rtm/resources/hg38.chrom.sizes \
+nevi_prev450k_mean_beta.bw
+
+~/myprograms/kentUtils/bin/linux.x86_64/bedGraphToBigWig nevus_train_mean_beta.bedgraph /home/rtm/resources/hg38.chrom.sizes \
+nevus_train_mean_beta.bw
+####################################################################################################################################
+computeMatrix reference-point \
+-S \
+nevus_train_mean_beta.bw \
+melanoma_train_mean_beta.bw \
+nevi_prev450k_mean_beta.bw \
+melanoma_prev450k_mean_beta.bw \
+-R /home/rtm/vivek/navi/meth_GEO/meth_nhm/poised_kmeans_zmet_5k_3clusters.bed --referencePoint center \
+--sortRegions descend -bs 20 -a 5000 -b 5000 -p max \
+-out /home/rtm/vivek/navi/meth_GEO/meth_nhm/melanoma_nevi_mean_beta_on_poised_kmeans_zmet_5k_3clusters.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "PoisedTSS" \
+--colorMap Reds \
+-m /home/rtm/vivek/navi/meth_GEO/meth_nhm/melanoma_nevi_mean_beta_on_poised_kmeans_zmet_5k_3clusters.mat \
+--samplesLabel "Nevi GSE120878" "Melanoma GSE120878" "Nevi GSE86355" "Melanoma GSE86355" \
+-out /home/rtm/vivek/navi/meth_GEO/meth_nhm/melanoma_nevi_mean_beta_on_poised_kmeans_zmet_5k_3clusters.pdf
+####################################################################################################################################
