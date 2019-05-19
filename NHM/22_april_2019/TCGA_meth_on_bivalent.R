9335c739fb3a425dbcc97cf6abfcd2e1b030bc96
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-20 01:20:47 +0800

    Update TCGA_meth_on_bivalent.R

--- NHM/22_april_2019/TCGA_meth_on_bivalent.R
+++ NHM/22_april_2019/TCGA_meth_on_bivalent.R
@@ -16,27 +16,56 @@ colnames(bg) = NULL
 rownames(bg) = NULL
 
 write.table(bg,"SKCM_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
-
+#######################################################################################################
+info = read.table("hm450.hg38.manifest.tsv",sep="\t", header=T)
+info = info[,c(1,2,3,5)]
 ######################################## TRAIN_SAMPLE ########################################
-info = read.table("hm450.hg38.manifest.tsv",sep="\t", header=T)
+GSE120878_anno = read.table("GSE120878_annotation.txt")
 x = readRDS("/home/rtm/vivek/navi/meth_GEO/GSE120878_beta.rds")
-meanx = rowMeans(x)
-info = info[,c(1,2,3,5)]
-ix = match(info[,4], names(meanx) )
-table( (info[,4]) == (names(meanx[ix])) )
-bg = cbind(info[,1:3],meanx[ix])
-colnames(bg) = NULL
-rownames(bg) = NULL
-write.table(bg,"train_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+track=as.character(GSE120878_anno[,5])
 
-######################################## prev 450k ########################################
-info = read.table("hm450.hg38.manifest.tsv",sep="\t", header=T)
-x = readRDS("/home/rtm/vivek/navi/meth_GEO/GSE86355_beta.rds")
-meanx = rowMeans(x)
-info = info[,c(1,2,3,5)]
+meanx = rowMeans(x[,track=="melanoma"])
 ix = match(info[,4], names(meanx) )
 table( (info[,4]) == (names(meanx[ix])) )
 bg = cbind(info[,1:3],meanx[ix])
 colnames(bg) = NULL
 rownames(bg) = NULL
-write.table(bg,"prev450k_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+bg = bg[!is.na(bg[,4]),]
+write.table(bg,"melanoma_train_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+
+meanx = rowMeans(x[,track=="nevus"])
+ix = match(info[,4], names(meanx) )
+table( (info[,4]) == (names(meanx[ix])) )
+bg = cbind(info[,1:3],meanx[ix])
+colnames(bg) = NULL
+rownames(bg) = NULL
+bg = bg[!is.na(bg[,4]),]
+write.table(bg,"nevus_train_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+######################################## prev 450k ########################################
+GSE86355_anno = read.table("GSE86355_anno.txt")
+x = readRDS("/home/rtm/vivek/navi/meth_GEO/GSE86355_beta.rds")
+track=as.character(GSE86355_anno[,2])
+
+meanx = rowMeans(x[,track=="MELANOMA"])
+ix = match(info[,4], names(meanx) )
+table( (info[,4]) == (names(meanx[ix])) )
+bg = cbind(info[,1:3],meanx[ix])
+colnames(bg) = NULL
+rownames(bg) = NULL
+bg = bg[!is.na(bg[,4]),]
+write.table(bg,"melanoma_prev450k_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+
+meanx = rowMeans(x[,track=="NEVI"])
+ix = match(info[,4], names(meanx) )
+table( (info[,4]) == (names(meanx[ix])) )
+bg = cbind(info[,1:3],meanx[ix])
+colnames(bg) = NULL
+rownames(bg) = NULL
+bg = bg[!is.na(bg[,4]),]
+write.table(bg,"nevi_prev450k_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+###############################################################################################################################
+
+
+
+
+
