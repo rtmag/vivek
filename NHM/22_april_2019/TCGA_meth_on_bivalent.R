624818dd33f70c4a8af8265c58efbdb2bdf1df62
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-20 00:23:23 +0800

    Update TCGA_meth_on_bivalent.R

--- NHM/22_april_2019/TCGA_meth_on_bivalent.R
+++ NHM/22_april_2019/TCGA_meth_on_bivalent.R
@@ -16,3 +16,27 @@ colnames(bg) = NULL
 rownames(bg) = NULL
 
 write.table(bg,"SKCM_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+
+######################################## TRAIN_SAMPLE ########################################
+info = read.table("hm450.hg38.manifest.tsv",sep="\t", header=T)
+x = readRDS("/home/rtm/vivek/navi/meth_GEO/GSE120878_beta.rds")
+meanx = rowMeans(x)
+info = info[,c(1,2,3,5)]
+ix = match(info[,4], names(meanx) )
+table( (info[,4]) == (names(meanx[ix])) )
+bg = cbind(info[,1:3],meanx[ix])
+colnames(bg) = NULL
+rownames(bg) = NULL
+write.table(bg,"train_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
+
+######################################## prev 450k ########################################
+info = read.table("hm450.hg38.manifest.tsv",sep="\t", header=T)
+x = readRDS("/home/rtm/vivek/navi/meth_GEO/GSE86355_beta.rds")
+meanx = rowMeans(x)
+info = info[,c(1,2,3,5)]
+ix = match(info[,4], names(meanx) )
+table( (info[,4]) == (names(meanx[ix])) )
+bg = cbind(info[,1:3],meanx[ix])
+colnames(bg) = NULL
+rownames(bg) = NULL
+write.table(bg,"prev450k_mean_beta.bedgraph",sep="\t",col.names=F,row.names=F,quote=F)
