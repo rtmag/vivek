9c2bd6f7bf92a7b8ae7460703af87d8879cac608
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-07-13 00:13:25 +0800

    Update 15_braf_calling.bash

--- NAVI/15_braf_calling.bash
+++ NAVI/15_braf_calling.bash
@@ -191,5 +191,18 @@ zgrep -P "chr17\t" *vcf.gz
 zgrep -P "chr17\t" *vcf.gz|cut -f1-7|perl -pe 's/\:/\t/g' > NF1_summary_noFILTER.txt
 
 
-
-
+--dbsnp /home/references/broadhg38/broad_hg38/dbsnp_146.hg38.vcf
+##################
+#ALL Variant NO NORMAL
+for bamfile in /home/rtm/vivek/navi/wes/bam/*_recalibrated.bam;
+do ls -lh $bamfile; 
+name=${bamfile//\/home\/rtm\/vivek\/navi\/wes\/bam\/} ;
+sample=${name//\_recalibrated\.bam} ;
+java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.jar Mutect2 \
+-R /home/references/broadhg38/broad_hg38/Homo_sapiens_assembly38.fasta \
+-I $bamfile \
+-tumor $sample \
+-O /home/rtm/vivek/navi/wes/test_vcf/all_$sample.vcf.gz
+done
+##################
+##################
