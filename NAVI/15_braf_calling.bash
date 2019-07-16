86c12e4dd40447f967f64df4cf4e343e63fcbb00
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-07-17 00:23:08 +0800

    Update 15_braf_calling.bash

--- NAVI/15_braf_calling.bash
+++ NAVI/15_braf_calling.bash
@@ -205,3 +205,15 @@ java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.
 done
 ##################
 ##################
+#ALL Variant NO NORMAL; On the WES_normal directory
+for bamfile in /home/rtm/vivek/navi/wes_normal/bam/VM*.recalibrated.bam;
+do ls -lh $bamfile; 
+name=${bamfile//\/home\/rtm\/vivek\/navi\/wes_normal\/bam\/} ;
+sample=${name//\.recalibrated\.bam} ;
+java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.jar Mutect2 \
+-R /home/references/broadhg38/broad_hg38/Homo_sapiens_assembly38.fasta \
+-I $bamfile \
+-tumor $sample \
+-O /home/rtm/vivek/navi/wes/test_vcf/all_$sample.vcf.gz
+done
+##################
