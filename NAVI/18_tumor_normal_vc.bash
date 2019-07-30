b25787387abbff6c0c5f3a7883274c07f223c8c2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-07-31 07:26:18 +0800

    Update 18_tumor_normal_vc.bash

--- NAVI/18_tumor_normal_vc.bash
+++ NAVI/18_tumor_normal_vc.bash
@@ -1,3 +1,9 @@
-java -jar /home/rtm/myprograms/gatk-4.1.2.0/gatk-package-4.1.2.0-local.jar Mutect2
-
-ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/Mutect2/
+java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.2.0/gatk-package-4.1.2.0-local.jar Mutect2 \
+-R /home/references/broadhg38/broad_hg38/Homo_sapiens_assembly38.fasta \
+-I /home/rtm/vivek/navi/wes_normal/bam/VM11.recalibrated.bam \
+-I /home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM11_VM12.recalibrated.bam \
+-tumor VM11 \
+-normal NORMAL_VM11_VM12 \
+--germline-resource /home/references/hg38_bundle/best_practice/af-only-gnomad.hg38_july_2019.vcf.gz \
+--af-of-alleles-not-in-resource 2.5e-06 \
+-O /home/rtm/vivek/navi/mutect2/VM11.uf.vcf.gz
