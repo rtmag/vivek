297b4b100f2494650139316060778a9d522cdc4c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-07-31 01:23:24 +0800

    Update 18_tumor_normal_vc.bash

--- NAVI/18_tumor_normal_vc.bash
+++ NAVI/18_tumor_normal_vc.bash
@@ -1,1 +1,3 @@
-java -jar /home/rtm/myprograms/gatk-4.1.2.0/gatk-package-4.1.2.0-local.jar
+java -jar /home/rtm/myprograms/gatk-4.1.2.0/gatk-package-4.1.2.0-local.jar Mutect2
+
+ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/Mutect2/
