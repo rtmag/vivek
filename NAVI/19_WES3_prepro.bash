fa44b3e4ebda895d7b39da61103daa2b2a52291d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-05 18:08:29 +0800

    Update 19_WES3_prepro.bash

--- NAVI/19_WES3_prepro.bash
+++ NAVI/19_WES3_prepro.bash
@@ -201,3 +201,15 @@ echo $samplename
 -bam /home/rtm/vivek/navi/wes3/bam/$samplename.sort.bam \
 -outfile /home/rtm/vivek/navi/wes3/bam/bamqc/$samplename.pdf --java-mem-size=200G ;
 done
+############################################################################################################################
+#### MUTECT VCF ALL Variant NO NORMAL
+for bamfile in /home/rtm/vivek/navi/wes3/bam/*recalibrated.bam;
+do ls -lh $bamfile; 
+name=${bamfile//\/home\/rtm\/vivek\/navi\/wes3\/bam\/} ;
+sample=${name//\.recalibrated\.bam} ;
+java -Xmx200G -jar /home/rtm/myprograms/gatk-4.1.0.0/gatk-package-4.1.0.0-local.jar Mutect2 \
+-R /home/references/broadhg38/broad_hg38/Homo_sapiens_assembly38.fasta \
+-I $bamfile \
+-tumor $sample \
+-O /home/rtm/vivek/navi/wes3/vcf_test/all_$sample.vcf.gz
+done
