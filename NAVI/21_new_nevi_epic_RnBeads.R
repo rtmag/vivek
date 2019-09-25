1260fb2ed8d851a3053ffe01c3e9a2a3c017afa0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-26 03:57:54 +0800

    Update 21_new_nevi_epic_RnBeads.R

--- NAVI/21_new_nevi_epic_RnBeads.R
+++ NAVI/21_new_nevi_epic_RnBeads.R
@@ -10,10 +10,64 @@ pre_sample_sheet_pilot = read.table(
     )
     
 colnames(pre_sample_sheet) = c("Sample_ID","Sentrix_ID","Sentrix_Position")
-write.csv(pre_sample_sheet,"new_nevi_epic_sample_sheet.csv",row.names=F)
-
+write.csv(pre_sample_sheet,"/home/rtm/vivek/navi/new_epic/new_nevi_epic_sample_sheet.csv",row.names=F)
 
 ## Options and Parameters ##
 
 #idat files
-idat.dir <- file.path("/home/rtm/vivek/navi/EPIC/idat_Raw_Data")
+idat.dir <- file.path("/home/rtm/vivek/navi/new_epic/Rawdata")
+
+# Sample annotation
+sample.annotation <- file.path("/home/rtm/vivek/navi/new_epic/new_nevi_epic_sample_sheet.csv")
+rnb.options(import.table.separator=",")
+
+system("rm -fr /home/rtm/vivek/navi/new_epic/RnBeads/RnBeads_report")
+report.dir <- file.path("/home/rtm/vivek/navi/new_epic/RnBeads/RnBeads_report")
+
+analysis.dir <- "/home/rtm/vivek/navi/new_epic/RnBeads/RnBeads_analysis"
+
+# Vanilla parameters
+rnb.options(filtering.sex.chromosomes.removal=TRUE, identifiers.column="Sample_ID")
+rnb.options(differential=FALSE)
+
+# Normalization parameters
+rnb.options(normalization=TRUE,normalization.method="swan",normalization.background.method="methylumi.noob")
+
+# Multiprocess
+num.cores <- 20
+parallel.setup(num.cores)
+
+rnb.run.analysis(dir.reports=report.dir, sample.sheet=sample.annotation, data.dir=idat.dir, data.type="infinium.idat.dir")
+
+###############################################################################################################################
+## preprocessing
+#idat files
+idat.dir <- file.path("/home/rtm/vivek/navi/new_epic/Rawdata")
+
+# Sample annotation
+sample.annotation <- file.path("/home/rtm/vivek/navi/new_epic/new_nevi_epic_sample_sheet.csv")
+rnb.options(import.table.separator=",")
+
+# Report directory
+system("rm -fr /home/rtm/vivek/navi/new_epic/RnBeads/RnBeads_normalization")
+report.dir <- file.path("/home/rtm/vivek/navi/new_epic/RnBeads/RnBeads_normalization")
+
+# Vanilla parameters
+rnb.options(identifiers.column="Sample_ID")
+
+# Multiprocess
+num.cores <- 20
+parallel.setup(num.cores)
+
+data.source <- c(idat.dir, sample.annotation)
+result <- rnb.run.import(data.source=data.source,data.type="infinium.idat.dir", dir.reports=report.dir)
+rnb.set.norm <- rnb.execute.normalization(result$rnb.set, method="swan",bgcorr.method="enmix.oob")
+
+save.rnb.set(rnb.set.norm,path="/home/rtm/vivek/navi/new_epic/RnBeads/RnBeads_normalization/rnb.set.norm.RData")
+###############################################################################################################################
+rnb.set.filtered <- rnb.execute.context.removal(rnb.set.norm)$dataset
+rnb.set.filtered <- rnb.execute.sex.removal(rnb.set.filtered)$dataset
+rnb.set.filtered <- rnb.execute.snp.removal(rnb.set.filtered, snp="any")$dataset
+rnb.set.filtered <- rnb.execute.na.removal(rnb.set.filtered)$dataset
+save.rnb.set(rnb.set.filtered,path="/home/rtm/vivek/navi/new_epic/RnBeads/RnBeads_normalization/rnb.set.norm.filtered.RData")
+#####################################################################################################################################
