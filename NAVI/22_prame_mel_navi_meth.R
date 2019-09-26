d59f62a517fc85eb686a81f16d08b5e743f53eea
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-27 00:17:04 +0800

    Update 22_prame_mel_navi_meth.R

--- NAVI/22_prame_mel_navi_meth.R
+++ NAVI/22_prame_mel_navi_meth.R
@@ -22,7 +22,7 @@ ix_cpg <- unique(c( grep("PRAME$",info$UCSC_RefGene_Name,perl=TRUE),
 info_prame <- info[ix_cpg,]
 info_prame <- info_prame[order(info_prame$Name),]
 
-beta <- meth(rnb.set.norm,row.names=T)
+beta <- meth(rnb.set.norm,row.names=TRUE)
 
 beta.prame <- beta[rownames(beta) %in% info_prame$Name,]
 meth.cent.prame <- meth.filtered.centered[rownames(meth.filtered.centered) %in% info_prame$Name,]
@@ -35,8 +35,8 @@ source('https://raw.githubusercontent.com/rtmag/tumor-meth-pipe/master/heatmap3.
 
 
 clab=as.character(rnb.set.norm@pheno$Tumor)
-clab[clab=="Melanoma"]="blue"
-clab[clab=="Nevi"]="red"
+clab[clab=="Melanoma"]="red"
+clab[clab=="Nevi"]="blue"
 clab[clab=="MIS"]="grey"
 
 png("heatmap-prame.png",width= 3.25,
@@ -45,5 +45,18 @@ png("heatmap-prame.png",width= 3.25,
 heatmap.2(as.matrix(meth.cent.prame),col=colors,scale="none", trace="none",
           distfun = function(x) get_dist(x,method="pearson"),srtCol=90,dendrogram='none',
 xlab="", ylab="",key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("Melanoma","Nevi","MIS"),fill=c("red","blue","grey"), border=T, bty="n" )
 dev.off()
 
+pdf("PRAME_methylationDifference_boxplot.pdf",height=8,width=14)
+data <- readRDS("diff_prame_meth.cent.prame.rds")
+data <- data[order(rowMeans(data)) , ] 
+data = data*2
+
+stripchart(as.data.frame(t(data)),vertical = TRUE,#jitter = 0.3, 
+          ylab = 'Methylation difference between matched Melanoma - Nevi for probes associated with PRAME',
+           method = "jitter", pch = 20, col = alpha(colour='red',alpha=.4),cex = 2,las=2)
+
+boxplot(t(data), add = TRUE,boxlwd = 2,las=2,outline=FALSE)
+abline(h=0)
+dev.off()
