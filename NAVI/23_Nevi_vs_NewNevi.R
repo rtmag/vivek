9794e58c78714062ba60ab2db8cae9eb170a453d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-30 18:28:06 +0800

    Update 23_Nevi_vs_NewNevi.R

--- NAVI/23_Nevi_vs_NewNevi.R
+++ NAVI/23_Nevi_vs_NewNevi.R
@@ -30,7 +30,33 @@ combined.rnb.set.norm@pheno$Tumor <- as.factor(combined.rnb.set.norm@pheno$Tumor
 
 nevidiff <- rnb.execute.computeDiffMeth(combined.rnb.set.norm,pheno.cols=c("Tumor"))
 comparison <- get.comparisons(nevidiff)[1]
-dmc_table <-get.table(nevidiff, comparison, "sites", return.data.frame=TRUE)
+dmc.Nevi.vs.non <-get.table(nevidiff, get.comparisons(nevidiff)[1], "sites", return.data.frame=TRUE)
+dmc.NN1.vs.non <-get.table(nevidiff, get.comparisons(nevidiff)[2], "sites", return.data.frame=TRUE)
+dmc.NN2.vs.non <-get.table(nevidiff, get.comparisons(nevidiff)[3], "sites", return.data.frame=TRUE)
+
+table(dmc.Nevi.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.Nevi.vs.non$mean.diff)>.25)
+table(dmc.NN1.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN1.vs.non$mean.diff)>.25)
+table(dmc.NN2.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN2.vs.non$mean.diff)>.25)
+
+# NEVI VS NN1
+rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$Tumor!="NewNevi2"])
+nevidiff.Nevi.NN1 <- rnb.execute.computeDiffMeth(rnb.set.tmp,pheno.cols=c("Tumor"))
+comparison <- get.comparisons(nevidiff.Nevi.NN1)[1]
+dmc.Nevi.vs.NN1 <-get.table(nevidiff.Nevi.NN1, comparison, "sites", return.data.frame=TRUE)
+
+# NEVI VS NN2
+rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$Tumor!="NewNevi1"])
+nevidiff.Nevi.NN2 <- rnb.execute.computeDiffMeth(rnb.set.tmp,pheno.cols=c("Tumor"))
+comparison <- get.comparisons(nevidiff.Nevi.NN2)[1]
+dmc.Nevi.vs.NN2 <-get.table(nevidiff.Nevi.NN2, comparison, "sites", return.data.frame=TRUE)
+
+# NN1 VS NN2
+rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$Tumor!="Nevi"])
+nevidiff.NN1.NN2 <- rnb.execute.computeDiffMeth(rnb.set.tmp,pheno.cols=c("Tumor"))
+comparison <- get.comparisons(nevidiff.NN1.NN2)[1]
+dmc.NN1.NN2 <-get.table(nevidiff.NN1.NN2, comparison, "sites", return.data.frame=TRUE)
+
+
 
 # VOLCANO
 
