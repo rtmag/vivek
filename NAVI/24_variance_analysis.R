ad3ac9fa1fe742f4409b4b5dd1151f81b7653013
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-05 00:15:32 +0800

    Update 24_variance_analysis.R

--- NAVI/24_variance_analysis.R
+++ NAVI/24_variance_analysis.R
@@ -10,11 +10,25 @@ options(bitmapType="cairo")
 rnb.set.norm=load.rnb.set("/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_normalization/rnb.set.norm.filtered.RData.zip")
 beta <- meth(rnb.set.norm,row.names=TRUE)
 
-meth.filtered.centered = readRDS('/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_differential/meth.norm.centered.rds')
-meth.filtered.centered<-meth.filtered.centered[rownames(meth.filtered.centered) %in% rownames(beta),]
+#meth.filtered.centered = readRDS('/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_differential/meth.norm.centered.rds')
+#meth.filtered.centered<-meth.filtered.centered[rownames(meth.filtered.centered) %in% rownames(beta),]
 
-meth.filtered.centered.sd <- apply(meth.filtered.centered,1,sd)
+#meth.filtered.centered.sd <- apply(meth.filtered.centered,1,sd)
 beta.sd <- apply(beta,1,sd)
+
+# 50,000
+beta.005<-beta[ order(beta.sd,decreasing=TRUE)[1:50000], ]
+
+column_ha = HeatmapAnnotation(Type = as.character(rnb.set.norm@pheno$Tumor),
+                              col = list(Type = c("Melanoma" = "red", "Nevi" = "blue", "MIS" = "grey") ) )
+
+pdf("SD_heatmap_top_50000_cpg.pdf",width=9)
+Heatmap(beta.005,
+show_row_names = FALSE,show_column_names = TRUE,name = "Methylation",row_dend_reorder = TRUE, column_dend_reorder = TRUE,
+column_title="Top 50000 CpGs with the highest SD", column_title_side = "bottom", row_title="", row_title_side = "right",
+top_annotation = column_ha, clustering_distance_columns = "pearson", clustering_distance_rows = "pearson")
+dev.off()
+##############
 
 # 0.5 %
 beta.005<-beta[ order(beta.sd,decreasing=TRUE)[1:round(length(beta.sd)*0.005)], ]
@@ -75,15 +89,5 @@ pdf("SD_heatmap_top_0.10_cpg.pdf",width=9)
 Heatmap(beta.005,
 show_row_names = FALSE,show_column_names = TRUE,name = "Methylation",row_dend_reorder = TRUE, column_dend_reorder = TRUE,
 column_title="Top 10% CpGs with the highest SD", column_title_side = "bottom", row_title="", row_title_side = "right",
-top_annotation = column_ha, clustering_distance_columns = "pearson", clustering_distance_rows = "pearson")
-dev.off()
-
-# 20%%
-beta.005<-beta[ order(beta.sd,decreasing=TRUE)[1:round(length(beta.sd)*0.2)], ]
-
-pdf("SD_heatmap_top_0.20_cpg.pdf",width=9)
-Heatmap(beta.005,
-show_row_names = FALSE,show_column_names = TRUE,name = "Methylation",row_dend_reorder = TRUE, column_dend_reorder = TRUE,
-column_title="Top 20% CpGs with the highest SD", column_title_side = "bottom", row_title="", row_title_side = "right",
 top_annotation = column_ha, clustering_distance_columns = "pearson", clustering_distance_rows = "pearson")
 dev.off()
