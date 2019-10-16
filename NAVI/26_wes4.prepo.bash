d3bf42ce080d3ee77c223c635f004c3e04245a96
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-17 02:44:24 +0800

    Create 26_wes4.prepo.bash

---
+++ NAVI/26_wes4.prepo.bash
@@ -0,0 +1,17 @@
+# Unzip all files
+unzip "*.zip"
+
+############################################################################################################################
+# Trimming
+
+for fastqfile in /home/rtm/vivek/navi/wes4/fastq/*_R1_001.fastq.gz ;
+do echo $fastqfile; 
+fastq2=$(echo $fastqfile|perl -pe "s/_R1_/_R2_/g") ;
+/home/rtm/myprograms/TrimGalore/trim_galore --paired --illumina -q 20 \
+--fastqc -o /home/rtm/vivek/navi/wes4/fastq_trim/ \
+$fastqfile \
+$fastq2 & ;
+done
+############################################################################################################################
+
+
