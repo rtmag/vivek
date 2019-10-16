74016b7434c4ef0fd0e2770c673e95e82e106ce0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-17 02:45:36 +0800

    Update 26_wes4.prepo.bash

--- NAVI/26_wes4.prepo.bash
+++ NAVI/26_wes4.prepo.bash
@@ -10,7 +10,7 @@ fastq2=$(echo $fastqfile|perl -pe "s/_R1_/_R2_/g") ;
 /home/rtm/myprograms/TrimGalore/trim_galore --paired --illumina -q 20 \
 --fastqc -o /home/rtm/vivek/navi/wes4/fastq_trim/ \
 $fastqfile \
-$fastq2 & ;
+$fastq2 &
 done
 ############################################################################################################################
 
