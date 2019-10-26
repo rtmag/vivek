497b9023d435609ea8b3a7fa3f2ca7bbe28a8aeb
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-27 03:10:51 +0800

    Create 27_matched_normal_mutect.bash

---
+++ NAVI/27_matched_normal_mutect.bash
@@ -0,0 +1,16 @@
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM1_VM2.recalibrated.bam
+/home/rtm/vivek/navi/wes4/bam/NORMAL_VM3_VM4.recalibrated.bam
+/home/rtm/vivek/navi/wes4/bam/NORMAL_VM5_VM6.recalibrated.bam
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM9_VM10.recalibrated.bam
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM11_VM12.recalibrated.bam
+/home/rtm/vivek/navi/wes4/bam/NORMAL_VM13_VM14.recalibrated.bam
+/home/rtm/vivek/navi/wes4/bam/NORMAL_VM17_VM18.recalibrated.bam
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM19_VM20.recalibrated.bam
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM21_VM22.recalibrated.bam
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM23_VM24.recalibrated.bam
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM35_VM36.recalibrated.bam
+/home/rtm/vivek/navi/wes4/bam/NORMAL_VM37_VM38.recalibrated.bam
+/home/rtm/vivek/navi/wes4/bam/NORMAL_VM39_VM40.recalibrated.bam
+/home/rtm/vivek/navi/wes4/bam/NORMAL_VM41_VM42.recalibrated.bam
+/home/rtm/vivek/navi/wes_normal/bam/NORMAL_VM43_VM44.recalibrated.bam
+/home/rtm/vivek/navi/wes3/bam/NORMAL_VM45_VM46.recalibrated.bam
