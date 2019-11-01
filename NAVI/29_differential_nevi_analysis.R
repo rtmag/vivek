61cf464279d161b953f3de33f4dabcdac79c8526
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-11-02 06:34:32 +0800

    Update 29_differential_nevi_analysis.R

--- NAVI/29_differential_nevi_analysis.R
+++ NAVI/29_differential_nevi_analysis.R
@@ -35,28 +35,28 @@ table(dmc.Nevi.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.Nevi.vs.non$mean.diff)>.
 table(dmc.NN1.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN1.vs.non$mean.diff)>.25)
 table(dmc.NN2.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN2.vs.non$mean.diff)>.25)
 
-table(dmc.Nevi.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.Nevi.vs.non$mean.diff)>.50)
-table(dmc.NN1.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN1.vs.non$mean.diff)>.50)
-table(dmc.NN2.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN2.vs.non$mean.diff)>.50)
+table(dmc.Nevi.vs.non$diffmeth.p.adj.fdr<0.01 & abs(dmc.Nevi.vs.non$mean.diff)>.40)
+table(dmc.NN1.vs.non$diffmeth.p.adj.fdr<0.01 & abs(dmc.NN1.vs.non$mean.diff)>.40)
+table(dmc.NN2.vs.non$diffmeth.p.adj.fdr<0.01 & abs(dmc.NN2.vs.non$mean.diff)>.40)
 
-N0vnull <- which(dmc.Nevi.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.Nevi.vs.non$mean.diff)>.5)
-N1vnull <- which(dmc.NN1.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN1.vs.non$mean.diff)>.5)
-N2vnull <- which(dmc.NN2.vs.non$diffmeth.p.adj.fdr<0.05 & abs(dmc.NN2.vs.non$mean.diff)>.5)
+N0vnull <- which(dmc.Nevi.vs.non$diffmeth.p.adj.fdr<0.01 & abs(dmc.Nevi.vs.non$mean.diff)>.40)
+N1vnull <- which(dmc.NN1.vs.non$diffmeth.p.adj.fdr<0.01 & abs(dmc.NN1.vs.non$mean.diff)>.40)
+N2vnull <- which(dmc.NN2.vs.non$diffmeth.p.adj.fdr<0.01 & abs(dmc.NN2.vs.non$mean.diff)>.40)
 
 Nvnull<-unique(c(N0vnull,N1vnull,N2vnull))
 length(Nvnull)
 
-beta <- meth(combined.rnb.set.norm,row.names=TRUE)
+#beta <- meth(combined.rnb.set.norm,row.names=TRUE)
 meth.norm.sig = beta[Nvnull,]
 meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
 
 column_ha = HeatmapAnnotation(Type = as.character(combined.rnb.set.norm@pheno$Tumor), 
                               col = list(Type = c("Nevi" = "blue", "NewNevi1" = "green", "NewNevi2" = "orange") ) )
 
-pdf("NEVI_differential_analysis_heatmap.pdf",width=9)
+pdf("NEVI_differential_analysis_heatmap_30methdiff.pdf",width=9)
 Heatmap(meth.norm.sig,
 show_row_names = FALSE,show_column_names = TRUE,name = "Methylation",row_dend_reorder = TRUE, column_dend_reorder = TRUE,
-column_title="", column_title_side = "bottom", row_title="", row_title_side = "right",column_km = 3, column_km_repeats = 100,
+column_title="", column_title_side = "bottom", row_title="", row_title_side = "right",km = 3,
 top_annotation = column_ha, clustering_distance_columns = "pearson", clustering_distance_rows = "pearson",col=colors)
 dev.off()
 
@@ -64,27 +64,27 @@ dev.off()
 ########################################################################################################################
 ########################################################################################################################
 # NN0 VS NN1
-rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$nevGroup=="NewNevi2"])
+rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$Tumor=="NewNevi2"])
 nevidiff.G1.G2 <- rnb.execute.computeDiffMeth(rnb.set.tmp,pheno.cols=c("Tumor"))
 comparison <- get.comparisons(nevidiff.G1.G2)[1]
 dmc.G1.vs.G2 <-get.table(nevidiff.G1.G2, comparison, "sites", return.data.frame=TRUE)
 
 # NN0 VS NN2
-rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$nevGroup=="NewNevi1"])
+rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$Tumor=="NewNevi1"])
 nevidiff.G1.G3 <- rnb.execute.computeDiffMeth(rnb.set.tmp,pheno.cols=c("Tumor"))
 comparison <- get.comparisons(nevidiff.G1.G3)[1]
 dmc.G1.vs.G3 <-get.table(nevidiff.G1.G3, comparison, "sites", return.data.frame=TRUE)
 
 # NN1 VS NN3
-rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$nevGroup=="Nevi"])
+rnb.set.tmp <- remove.samples(combined.rnb.set.norm,samples(combined.rnb.set.norm)[combined.rnb.set.norm@pheno$Tumor=="Nevi"])
 nevidiff.G2.G3 <- rnb.execute.computeDiffMeth(rnb.set.tmp,pheno.cols=c("Tumor"))
 comparison <- get.comparisons(nevidiff.G2.G3)[1]
 dmc.G2.vs.G3 <-get.table(nevidiff.G2.G3, comparison, "sites", return.data.frame=TRUE)
 
 
-table(dmc.G1.vs.G2$diffmeth.p.adj.fdr<0.05 & abs(dmc.G1.vs.G2$mean.diff)>.5)
-table(dmc.G1.vs.G3$diffmeth.p.adj.fdr<0.05 & abs(dmc.G1.vs.G3$mean.diff)>.5)
-table(dmc.G2.vs.G3$diffmeth.p.adj.fdr<0.05 & abs(dmc.G2.vs.G3$mean.diff)>.5)
+table(dmc.G1.vs.G2$diffmeth.p.adj.fdr<0.05 & abs(dmc.G1.vs.G2$mean.diff)>.3)
+table(dmc.G1.vs.G3$diffmeth.p.adj.fdr<0.05 & abs(dmc.G1.vs.G3$mean.diff)>.3)
+table(dmc.G2.vs.G3$diffmeth.p.adj.fdr<0.05 & abs(dmc.G2.vs.G3$mean.diff)>.3)
 # VOLCANO
 
 png("volcano_Nevi.vs.NewNevi1.png",width= 3.25,
