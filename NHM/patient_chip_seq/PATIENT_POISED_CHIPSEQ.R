5f6c24e8400c3e10c4969e10c20d8d676d09f6b0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-02-03 18:31:38 +0800

    Update PATIENT_POISED_CHIPSEQ.R

--- NHM/patient_chip_seq/PATIENT_POISED_CHIPSEQ.R
+++ NHM/patient_chip_seq/PATIENT_POISED_CHIPSEQ.R
@@ -1,4 +1,11 @@
+library(ComplexHeatmap)
+options(scipen=999)
+library(graphics)
+library(gplots)
+library(RColorBrewer)
+
 # #cluster_1:556	cluster_2:438	cluster_3:1180
+
 
 fresh_k27 <- read.table(pipe("tail -n +4 patient_fresh_H3K27me3.txt"),sep="\t")
 
@@ -24,3 +31,19 @@ fresh_k27_means <- cbind(rowMeans(fresh_k27[,1:2]),
       rowMeans(fresh_k27[,39:40])
       )
       
+
+Heatmap(fresh_k27_means, row_split = c( rep("c1",556), rep("c2",438), rep("c3",1180) ) ,
+        show_row_names = FALSE,show_column_names = FALSE, 
+        column_title="H3K27me3 Patients", 
+        cluster_columns=F,cluster_rows=F)
+        
+        
+        
+show_row_names = FALSE,show_column_names = FALSE,name = "Expression",row_dend_reorder = T, column_dend_reorder = F,
+column_title="Freije Patients", column_title_side = "bottom", row_title="Gene Signature", row_title_side = "right",
+bottom_annotation = column_ha, right_annotation = row_ha,
+        clustering_distance_columns = "pearson",column_km = 3, column_km_repeats = 100,
+        clustering_distance_rows = "pearson",row_split =track)
+
+
+ht1 + ht2
