43fa0b3b9942c0d8dc1110caa55af70001af9d68
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-02-04 15:49:35 +0800

    Create genomebrowser_meth_plots.R

---
+++ NAVI/genomebrowser_meth_plots.R
@@ -0,0 +1,6 @@
+############################################################################################
+suppressMessages(library(RnBeads))
+options(bitmapType="cairo")
+options(scipen=999)
+############################################################################################
+rnb.set.norm=load.rnb.set("/home/rtm/vivek/navi/EPIC_2nd_batch/RnBeads/RnBeads_normalization/rnb.set.norm.RData.zip")
